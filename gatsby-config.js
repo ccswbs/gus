d949f45ad73440d50605a7115ecccc5db1cb4e32
Miranda Mafe <miranda.mafe@gmail.com>
2019-12-05 15:49:54 -0500

    Add gatsby image, manifest, react-helmet, and transformer-sharp plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,26 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "Gatsby UG Starter Template",
+    description: "Description of the site",
+    author: "Author of site",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby UG Starter Template`,
+        short_name: `UG starter`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#000000`,
+        display: `browser`,
+        icon: `src/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
 }
