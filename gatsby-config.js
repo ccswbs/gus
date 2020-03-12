2819a410bf46acb113aa1e51526de4da34439281
Miranda Mafe <miranda.mafe@gmail.com>
2020-03-12 13:45:46 -0400

    Use _gatsby_source in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
     {
        resolve: `gatsby-source-drupal`,
        options: {
-         baseUrl: `https://api.` + process.env._ENV + process.env._SITE + `.` + process.env._ZONE + `/`,
+         baseUrl: `https://api.` + process.env._GATSBY_SOURCE + process.env._SITE + `.` + process.env._ZONE + `/`,
          apiBase: `jsonapi`, // optional, defaults to `jsonapi`
        },
     },
