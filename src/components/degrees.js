625b49a603795611fb9968c77772f0050bec1864
Tanya Qureshi <tqureshi@uoguelph.ca>
2020-04-16 13:15:50 -0400

    add degrees component

---
+++ src/components/degrees.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Link, StaticQuery, graphql } from "gatsby"
+
+export default () => (
+	<><h2>Degrees</h2>
+	<ul>
+	  <StaticQuery
+		query={degreeQuery}
+		render={data => (
+		  data.allTaxonomyTermDegrees.edges.map(edge => (
+			<li><Link to={edge.node.path.alias}>{edge.node.field_degree_acronym}</Link></li>))
+		)}
+	  />
+	</ul></>
+)
+
+const degreeQuery = graphql`
+query MyQuery {
+	  allTaxonomyTermDegrees {
+		edges {
+		  node {
+			drupal_internal__tid
+			field_degree_acronym
+			path {
+			  alias
+			}
+		  }
+		}
+	  }
+	}
+`
