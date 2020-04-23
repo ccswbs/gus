398ad1812f0999ff8f6591800f39fb05bc42d04f
Tanya Qureshi <tqureshi@uoguelph.ca>
2020-04-23 16:24:35 -0400

    fix for null values

--- src/components/units.js
+++ src/components/units.js
@@ -2,21 +2,30 @@ import React from "react"
 import PropTypes from 'prop-types'
 
 function Units ({ unitData }) {
-	var units;
-	var unitList="";
-	if (unitData !== null) {
-		for (var i=0; i < unitData.length; i++) {
-				unitList += "<li>" + unitData[i].relationships.field_units[i].name + " (" + unitData[i].relationships.field_units[i].field_unit_acronym + ")</li>";
-			}
-		units = "<h2>Departments</h2><ul>" + unitList + "</ul>";
-		return <div dangerouslySetInnerHTML={{__html: units}}/>
-	} else {
-		return null
-	}
+    var units;
+    var unitList="";
+    if (unitData !== null) {
+        unitData.forEach(element => {
+            const field_units = element.relationships.field_units; 
+            field_units.forEach(unit => {
+                const acronym = (unit.field_unit_acronym !== undefined && unit.field_unit_acronym !== null ? ` (` + unit.field_unit_acronym + `)`: ``);
+                unitList += "<li>" + unit.name + acronym + "</li>";
+            });
+        });
+        if (unitList !== ""){
+            units = "<h2>Departments</h2><ul>" + unitList + "</ul>";
+            return <div dangerouslySetInnerHTML={{__html: units}}/>
+        }
+        
+    }
+    return null;
 }
 
 Units.propTypes = {
-	unitData: PropTypes.arrayOf(PropTypes.string),
+    unitData: PropTypes.arrayOf(PropTypes.string),
 }
-
+Units.defaultProps = {
+    unitData: null,
+  }
+  
 export default Units
