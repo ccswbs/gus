579989f5be28e7502f6fdec1ed3d593edb760de1
Miranda Mafe <miranda.mafe@gmail.com>
2020-05-27 17:17:24 -0400

    Add callToAction component

--- src/templates/program-page.js
+++ src/templates/program-page.js
@@ -5,15 +5,20 @@ import SEO from '../components/seo'
 import Degrees from '../components/degrees'
 import CallToAction from '../components/callToAction'
 
+
 export default ({data, location}) => {
-  // determine source of query
   var pageData;
-	var degreesData;
+  var degreesData;
+  var callToActionData;
 
   if(data.programs.edges[0] !== undefined){
     pageData = data.programs.edges[0].node;
   }
 
+  if(data.ctas.edges[0] !== undefined){
+    callToActionData = data.ctas.edges;
+  }
+  
 	// set program info
 	const title = pageData.name;
 	const description = (pageData.description !== undefined 
@@ -21,7 +26,7 @@ export default ({data, location}) => {
 	const acronym = (pageData.acronym !== undefined && pageData.acronym !== null ? `(` + pageData.acronym + `)`: ``);
 
 	// set degree info  
-	degreesData = pageData.relationships.field_degrees;
+  degreesData = pageData.relationships.field_degrees;
 
 	return (
 		<Layout>
@@ -29,14 +34,22 @@ export default ({data, location}) => {
 		  <h1>{title} {acronym}</h1>
 		  <div dangerouslySetInnerHTML={{ __html: description }}  />
 		  <Degrees degreesData={degreesData} />
-      <CallToAction href='#'>Apply</CallToAction>
+
+      {callToActionData.map((cta, index) => (
+        <CallToAction key={index} href={cta.node.field_call_to_action_link.uri} 
+            goalEventCategory={cta.node.relationships.field_call_to_action_goal.name} 
+            goalEventAction={cta.node.relationships.field_call_to_action_goal.field_goal_action} >
+          {cta.node.field_call_to_action_link.title}
+        </CallToAction>
+      ))}
+      
 		</Layout>
 	)
 }
 
 export const query = graphql`
-  query ($id: String!) {
-    programs: allTaxonomyTermPrograms(filter: {drupal_id: {eq: $id}}) {
+  query ($id: String) {
+    programs: allTaxonomyTermPrograms(filter: {id: {eq: $id}}) {
       edges {
         node {
           drupal_id
@@ -49,6 +62,31 @@ export const query = graphql`
             field_degrees {
               name
               field_degree_acronym
+            }
+          }
+        }
+      }
+    }
+
+    ctas: allNodeCallToAction(filter: {fields: {tags: {in: [$id] }}}) {
+      edges {
+        node {
+          field_call_to_action_link {
+            title
+            uri
+          }
+          relationships {
+            field_call_to_action_goal {
+              name
+              field_goal_action
+            }
+            field_tags {
+              __typename
+              ... on TaxonomyInterface {
+                drupal_id
+                id
+                name
+              }
             }
           }
         }
