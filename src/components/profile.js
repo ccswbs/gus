cbd648c15ad09584d1b8b19f02ee199a6a29eeb6
J. Banerji <abanerji@uoguelph.ca>
2020-06-02 17:57:18 +0000

    Added profile.js as component

---
+++ src/components/profile.js
@@ -0,0 +1,59 @@
+import React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from "gatsby-image"
+
+export default function Profile() {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allNodeStudentProfile {
+            nodes {
+                body {
+                    value
+                    processed
+                }
+                title
+                field_program_year
+                field_picture {
+                    alt
+                }
+                relationships {
+                    field_picture {
+              
+                        localFile {
+                            url
+                            childImageSharp {
+                                fluid(maxWidth: 400, maxHeight: 250) {
+                                    originalImg
+                                    ...GatsbyImageSharpFluid
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    `
+  )
+  
+  return (
+    <div> 
+    <h2>Student Profiles</h2>  
+    
+    {data.allNodeStudentProfile.nodes.map(node => (
+      <div>
+
+          {node.title} <br></br>
+          <Img fluid={node.relationships.field_picture.localFile.childImageSharp.fluid} alt="" />
+          
+          <p dangerouslySetInnerHTML={{ __html: node.body.value }} />
+          
+          
+      </div> 
+    ))}
+    
+    </div>
+  )
+}
+
