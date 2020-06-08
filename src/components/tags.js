0d4273c80b03d00edda40769232e56375dfc0b61
Tanya Qureshi <tqureshi@uoguelph.ca>
2020-06-08 14:02:17 -0400

    Resolving PR comments

--- src/components/tags.js
+++ src/components/tags.js
@@ -1,16 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
+import "../styles/tags.css"
 
 function Tags ({ tagData }) {
 	var tags = "";
 	var tagsList = "";
     if (tagData !== null) {
 		tagData.forEach(tag => {
-			tagsList += "#" + tag.name + " ";
+			tagsList += "<li>" + "#" + tag.name + "</li>";
 		});
 		if (tagsList !== "") {
-			tags = tagsList;
-			return <div id="tags"><div class="container" dangerouslySetInnerHTML={{__html: tags}} /></div>
+			tags = "<ul class='container'>" + tagsList + "</ul>";
+			return <div id="tags" dangerouslySetInnerHTML={{__html: tags}} />
 		}		
     }
     return null;
