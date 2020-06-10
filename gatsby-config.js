1c4ecf14db652d1bf8bdfafd29a8b4ecbddec157
Miranda Mafe <miranda.mafe@gmail.com>
2020-06-10 10:57:48 -0400

    Revert gatsby-config to pipeline variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-// let metaConfig = require('./config/sites/' + process.env._SITE + '.js');
-let metaConfig = require('./config/sites/ugconthub.js');
+let metaConfig = require('./config/sites/' + process.env._SITE + '.js');
 
 if((metaConfig == null) || (metaConfig == undefined)) {
   metaConfig['title'] = "Gatsby UG Starter Template";
@@ -46,8 +45,7 @@ module.exports = {
     {
        resolve: `gatsby-source-drupal`,
        options: {
-        //  baseUrl: `https://api.` + process.env._GATSBY_SOURCE + process.env._SITE + `.` + process.env._ZONE + `/`,
-         baseUrl: `https://cta-drupal-bovey.pantheonsite.io/`,
+         baseUrl: `https://api.` + process.env._GATSBY_SOURCE + process.env._SITE + `.` + process.env._ZONE + `/`,
          apiBase: `jsonapi`, // optional, defaults to `jsonapi`
        },
     },
