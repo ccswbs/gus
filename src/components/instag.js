b6244ba61159d677ca5c3c6e33f41b9ed65c0140
Miranda Mafe <miranda.mafe@gmail.com>
2020-06-11 15:40:11 -0400

    Add key to instagram

--- src/components/instag.js
+++ src/components/instag.js
@@ -6,11 +6,11 @@ class Instagram extends React.Component {
       return (
         <React.Fragment>
           {this.props.instaNodes.edges && (
-            <div className="ug-instagram">
-              {this.props.instaNodes.edges.map((edge) => {
-                return <p><img src={edge.node.original} alt="" width="250" height="250" /> {edge.node.caption} </p>
+            <ul className="ug-instagram">
+              {this.props.instaNodes.edges.map((edge, index) => {
+                return <li key={index}><img src={edge.node.original} alt="" width="250" height="250" /> {edge.node.caption} </li>
               })}
-            </div>
+            </ul>
           )}
         </React.Fragment>
       )
