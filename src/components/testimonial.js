5cc67f01b42fffd061296db437106df5425f19f0
Miranda Mafe <miranda.mafe@gmail.com>
2020-07-28 17:06:55 -0400

    Convert testimonials to slick carousel

--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -1,21 +1,52 @@
 import React from "react"
 import PropTypes from 'prop-types'
 import Img from "gatsby-image"
+import Slider from "react-slick";
 import { stripHTMLTags } from '../utils/ug-utils.js'
 import "../styles/testimonial.css"
+import "slick-carousel/slick/slick.css";
+
+function CarouselArrow (props) {
+	const { onClick } = props;
+	let arrowText = props.type === "next" ? "Next" : "Previous";
+	let classNames = props.type === "next" ? "ug-slick-carousel-control-next" : "ug-slick-carousel-control-prev";
+	let classNameIcon = classNames + "-icon ug-slick-carousel-control-arrow-icon";
+	classNames += " ug-slick-carousel-control-arrow";
+
+	return (
+	  	<button className={classNames} onClick={onClick}>
+			<span className={classNameIcon} aria-hidden="true"></span>
+			<span className="sr-only">{arrowText}</span>
+		</button>
+	);
+  }
 
 function Testimonials ({ testimonialData, heading }) {
 
+    const sliderSettings = {
+		nextArrow: <CarouselArrow type="next" />,
+		prevArrow: <CarouselArrow type="prev" />,
+		dots: false,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false
+				}
+			}
+		]
+	  };
+
 	if ((testimonialData !== null) && (testimonialData !== undefined)) {
-
-		const numPerRow = 2;
-		var testimonialRow = [];
-
-		const testimonialUnits = testimonialData.map((testimonial, index) => {
+		const testimonialUnits  = () => testimonialData.map((testimonial, index) => {
 			var testimonialContent = stripHTMLTags(testimonial.node.body.value);
-			var testimonialClass = ((index % 2 === 0) ? `left` : `right`);
 			var testimonialPicture = testimonial.node.relationships.field_picture;
-			return <div className={testimonialClass} key={testimonial.node.drupal_id}>
+			return <div className="ug-slick-item" key={testimonial.node.drupal_id}>
 				{testimonialPicture && <Img fluid={testimonialPicture.localFile.childImageSharp.fluid} alt={testimonial.node.relationships.field_picture.alt} />}
 				<blockquote dangerouslySetInnerHTML={{__html: testimonialContent}} />
 				<p className="tagline">
@@ -26,18 +57,6 @@ function Testimonials ({ testimonialData, heading }) {
 			</div>
 		})
 
-		for (var i = 0; i < testimonialUnits.length; i+=numPerRow) {
-			const testimonialRowClasses = (i === 0) ? "carousel-item active" : "carousel-item";
-			testimonialRow.push(
-				<div key={`testimonial-row-id-` + i} className={testimonialRowClasses}>
-					<div className="testimonial-wrapper">
-						{testimonialUnits[i]}
-						{testimonialUnits[i+1]}
-					</div>
-				</div>
-			)
-		 }
-
 		return (
 			<div className="ug-testimonial">
 				<div className="full-width-container bg-light">
@@ -45,21 +64,10 @@ function Testimonials ({ testimonialData, heading }) {
 						<section className="row row-with-vspace site-content">
 							<div className="col-md-12 content-area" id="main-column">
 								<h3 className="carousel-header">{heading}</h3>
-								<div id="carouselExampleControls" className="carousel slide" data-interval="false">
-									
-									<div className="carousel-inner">
-										{testimonialRow.map((row) => {
-											return row
-										})}
+								<div className="ug-slick-carousel">
+									<div className="testimonial-wrapper">
+										<Slider {...sliderSettings}>{testimonialUnits()}</Slider>
 									</div>
-									<a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-										<span className="carousel-control-prev-icon" aria-hidden="true"></span>
-										<span className="sr-only">Previous</span>
-									</a>
-									<a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-										<span className="carousel-control-next-icon" aria-hidden="true"></span>
-										<span className="sr-only">Next</span>
-									</a>
 								</div>
 							</div>
 						</section>
