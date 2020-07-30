34ee41b21c73b2c4cde5352669ec8a8574b22391
Miranda Mafe <miranda.mafe@gmail.com>
2020-07-30 15:40:43 -0400

    Add last updated component

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,22 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import SkipLink from './skiplink'
 import Header from './header'
+import DateModified from '../components/datemodified'
 import Footer from './footer'
 import "../styles/global.css"
 
-const Layout = ({ children }) => (
+const Layout = ({ children, date }) => (
       <>
         <SkipLink mainContent="#content"/>
         <Header />
         <main className="main-container">
           {children}
         </main>
+        <DateModified date={date}/>
         <Footer />
       </>
 )
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  date: PropTypes.string,
 }
 
 export default Layout
