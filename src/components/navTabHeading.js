97f069d42e1015a727096414edb02f7813d65450
Miranda Mafe <miranda.mafe@gmail.com>
2020-08-05 17:00:32 -0400

    Add navTabs, navTabContent, and navTabHeading components

---
+++ src/components/navTabHeading.js
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+/* 
+Example Usage:
+    <NavTabs classNames="optional-class" headings={
+        <>
+            <NavTabHeading active={true} heading="Tab Heading 1" controls="tab1" />
+            <NavTabHeading heading="Tab Heading 2" controls="tab2" />
+            <NavTabHeading heading="Tab Heading 3" controls="tab3" />
+        </>
+        }>
+            <NavTabContent active={true} id="tab1" content={<div>Test 1</div>} classNames="optional-class" />
+            <NavTabContent id="tab2" content={<div>Test 2</div>} classNames="optional-class" />
+            <NavTabContent id="tab3" content={<div>Test 3</div>} classNames="optional-class" />
+    </NavTabs>
+*/
+
+function NavTabHeading (props) {
+    let id = `${props.controls}-tab`;
+    let href = `#${props.controls}`;
+    let classes = (props.active === true) ? `active nav-item nav-link`:`nav-item nav-link`;
+    if(props.classNames !== ``){
+        classes += ` ${props.classNames}`;
+    }
+
+    return(
+            <a class={classes} 
+                id={id} 
+                href={href} 
+                role="tab" 
+                data-toggle="pill" 
+                aria-controls={props.controls} 
+                aria-selected={props.active}>
+                {props.heading}
+            </a>
+    )
+}
+
+NavTabHeading.propTypes = {
+    children: PropTypes.node.isRequired,
+    classNames: PropTypes.string,
+}
+  
+NavTabHeading.defaultProps = {
+    children: ``,
+    classNames: ``,
+  }
+
+export default NavTabHeading
---
+++ src/components/navTabs.js
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+/* 
+Example Usage:
+    <NavTabs classNames="optional-class" headings={
+        <>
+            <NavTabHeading active={true} heading="Tab Heading 1" controls="tab1" />
+            <NavTabHeading heading="Tab Heading 2" controls="tab2" />
+            <NavTabHeading heading="Tab Heading 3" controls="tab3" />
+        </>
+        }>
+            <NavTabContent active={true} id="tab1" content={<div>Test 1</div>} classNames="optional-class" />
+            <NavTabContent id="tab2" content={<div>Test 2</div>} classNames="optional-class" />
+            <NavTabContent id="tab3" content={<div>Test 3</div>} classNames="optional-class" />
+    </NavTabs>
+*/
+
+function NavTabs (props) {
+    const classes = `nav nav-pills nav-justified ${props.classNames}`
+
+    return(
+        <>
+            <nav class={classes}>
+                {props.headings}
+            </nav>
+            <div class="tab-content" id="pills-tabContent">
+                {props.children}
+            </div>
+        </>
+    )
+}
+
+NavTabs.propTypes = {
+    children: PropTypes.node.isRequired,
+    classNames: PropTypes.string,
+    headings: PropTypes.array,
+}
+  
+NavTabs.defaultProps = {
+    children: ``,
+    classNames: ``,
+    headings: null,
+    propTypes: null,
+  }
+
+export default NavTabs
