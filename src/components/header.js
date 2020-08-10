23f4e7c1371a2a8f4fe883cc35a3c3a8163b1167
Miranda Mafe <mmafe@uoguelph.ca>
2020-08-10 18:25:54 +0000

    Merged PR 39: Add native header web component
    # Summary of Changes
    - Add native U of G header web component, courtesy of @<Raza Mahmood>/C&PA
    
    ## Pull Request Checklist
    
    ### Is Content created by the story?
    - [ ] No
    - [x] If yes, the content is:
       - [x] Accessible
       - [x] Mobile-friendly
       - [x] Version-controlled
       - [x] Governed by appropriate roles and permissions
       - [x] Organized by taxonomy
    
    ### Are there User-facing changes in the Drupal UI?
    - [x] No
    
    ### Are there (Drupal) Upstream Code changes?
    - [x] No
    
    ### Are there Gatsby Code changes?
    - [ ] No
    - [x] If yes, then:
       - [x] Code is complete and commented.
       - [x] Code is secure to the best of our knowledge
       - [x] Remove or limit console errors and warnings
       - [x] Gatsby page renders whether affiliated Drupal content exists or not (ensure there is an appropriate schema for the content)
       - Is a test necessary?
          - [x] No
       - [x] Pull request (PR) is mergeable with *develop* branch
       - [x] PR has a meaningful title
       - [x] PR has a summary of changes
       - [x] PR is peer reviewed and meets team standards.
       - [x] PR builds without error

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 const Header = () => (
-  <header>
-      <p>Header Element</p>
-  </header>
+    <>
+        <div id="header-breakpoint"></div>
+        <uofg-header  class="unloaded">
+            <a href="#">Apply</a>
+            <uofg-dropdown-menu>
+            <span className="opener">Dropdown Example</span>
+                <ul>
+                    <li><a href="#">Menu item 1</a></li>
+                    <li><a href="#">Menu item 2</a></li>
+                    <li><a href="#">Menu item 3</a></li>
+                    <li><a href="#">Menu item 4</a></li>
+                    <li><a href="#">Menu item 5</a></li>
+                    <li><a href="#">Menu item 6</a></li>
+                </ul>
+            </uofg-dropdown-menu>
+        </uofg-header>
+    </>
 )
 
 export default Header
