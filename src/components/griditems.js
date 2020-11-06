fd8c799397c5623739daa988ad19e0dc77e4a4d0
snschau <SSchau@uoguelph.ca>
2020-11-06 13:35:21 -0500

    quick commit too get miranda's help

--- src/components/griditems.js
+++ src/components/griditems.js
@@ -1,39 +1,51 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import { contentExists  } from '../utils/ug-utils';
-import '../styles/stats.css';
-import GridItem from './griditem'
+import React from 'react';
+import Grid from './grid';
+import GridCell from './gridCell';
+import { Link } from 'gatsby';
+import { contentExists } from '../utils/ug-utils';
 
+function GridItems (props) {
 
-function GridItems (gridItemsData) {
-if (contentExists(gridItemsData)){
-	// console.log("gridItemsData")
-	// console.log(gridItemsData)
-	// console.log(gridItemsData.pageData)
-	// console.log(gridItemsData.pageData.length)
-	// console.log(contentExists(gridItemsData.pageData))
-	// console.log(gridItemsData.pageData.length !== 0)
+    if(contentExists(props.pageData) && props.pageData.length !== 0){
+		return (
+			<Grid key={props.pageData.drupal_id}>
+				{props.pageData.map (paragraph  => {
+					if(contentExists(paragraph.relationships)){
+						let gridItem = paragraph.relationships;
+						console.log(gridItem)
+						console.log(gridItem.field_grid_image)
+						console.log(gridItem.field_grid_image.relationships.field_media_image)
+							
+						const image = (contentExists(gridItem.field_grid_image)) ? gridItem.field_grid_image.relationships.field_media_image : null;
+						const imageFile = (contentExists(image)) ? <img src={image.localFile.childImageSharp.resize.src} alt="" /> : null;
+						return <GridCell key={gridItem.drupal_id} 
+									// url={gridItem.fields.alias.value} 
+									image={imageFile} 
+									heading={gridItem.title}
+									headingLevel="h3" />
+						
+						
+					}
+					return null;
+				})}
+			</Grid>
+		)
+        
+    }
 
-	return <React.Fragment>
-	<div className="full-width-container stats-bg">
-		<div className="container page-container">
-			<section className="row row-with-vspace site-content">
-				<div className="col-md-12 content-area"> 
-					<h2 className="sr-only">Grid Place holder</h2>
-					<GridItem gridItemsData={gridItemsData}/>
-				</div>
-			</section>
-		</div>
-	</div>
-	
-	</React.Fragment>
-}
+    return null;
+
 }
 
 GridItems.propTypes = {
-	gridItemsData: PropTypes.array,
+    pageData: PropTypes.array,
+    displayType: PropTypes.string,
 }
+  
 GridItems.defaultProps = {
-	gridItemsData: null,
-}
+    pageData: ``,
+    displayType: `list`,
+  }
+
 export default GridItems
