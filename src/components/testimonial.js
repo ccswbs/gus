3ea8dfbd81a61f76234b2bb45e9c889f40d7cc2d
snschau <SSchau@uoguelph.ca>
2020-11-11 16:20:08 -0500

    Testimonials, display 1 or many, w/ or w/out image

--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -2,16 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 import SliderResponsive from './sliderResponsive';
-import { stripHTMLTags, setHeadingLevel } from '../utils/ug-utils.js';
+import { contentExists, stripHTMLTags, setHeadingLevel } from '../utils/ug-utils.js';
 import '../styles/testimonial.css';
+
 
 function Testimonials (props) {
 	let Heading = setHeadingLevel(props.headingLevel);
 
 	if ((props.testimonialData !== null) && (props.testimonialData !== undefined)) {
-		const testimonialUnits  = () => props.testimonialData.map((testimonial, index) => {
+		const testimonialUnits  = () => props.testimonialData.map((testimonial) => {
+
 			let testimonialContent = stripHTMLTags(testimonial.node.body.processed);
-			let testimonialPicture = testimonial.node.relationships.field_hero_image.relationships.field_media_image;
+			let testimonialPicture = (contentExists(testimonial.node.relationships.field_hero_image)) ? testimonial.node.relationships.field_hero_image.relationships.field_media_image : null;
+
 			return <div key={testimonial.node.drupal_id}>
 				{testimonialPicture && <Img className="testimonial-pic" fluid={testimonialPicture.localFile.childImageSharp.fluid} alt={testimonial.node.relationships.field_hero_image.field_media_image.alt} />}
 				<blockquote className="testimonial-quote" dangerouslySetInnerHTML={{__html: testimonialContent}} />
