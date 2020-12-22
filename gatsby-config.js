6cddd3bd84a8d760b3920544566726a370ef0f22
M. Brent Harp <brharp@live.com>
2020-12-22 11:21:51 -0500

    Set menu source from variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
 	{
       resolve: `gatsby-source-drupal-menu-links`,
       options: {
-		baseUrl: `https://api.` + process.env._GATSBY_SOURCE + process.env._SITE + `.` + process.env._ZONE,
+		baseUrl: `https://api.` + _env + _site + `.` + _zone,
 		/** For dev purposes, comment the line above and uncomment the appropriate line below: **/
 		//baseUrl: `https://api.devugconthub.uoguelph.dev`,
 		//baseUrl: `https://api.liveugconthub.uoguelph.dev`,
