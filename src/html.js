c7bc044022238701455a0ca451028b17854ac494
Tanya Qureshi <tqureshi@uoguelph.ca>
2021-01-18 16:36:12 -0500

    update to be consistent with latest develop changes

--- src/html.js
+++ src/html.js
@@ -13,8 +13,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-        <script defer src="/assets/UofGHeader.js"></script>
-        <link rel="stylesheet" href="/assets/UofGHeader.css"></link>
+		<script defer src="https://www.uoguelph.ca/includes/web-components/UofGHeader-dist.js"></script>
       </head>
       <body {...props.bodyAttributes}>
         <SkipLink mainContent="#content"/>
