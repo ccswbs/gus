ea6f2fa2db73e089b9832f37516e2bc532317527
Tanya Qureshi <tqureshi@uoguelph.ca>
2021-03-02 10:01:35 -0500

    rearrange external js files

--- src/html.js
+++ src/html.js
@@ -14,7 +14,8 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-        <script defer src="https://www.uoguelph.ca/includes/web-components/UofGHeader-dist.js"></script>
+		<script data-search-pseudo-elements defer src="https://kit.fontawesome.com/7993323d0c.js" crossOrigin="anonymous"></script>
+		<script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.4.4/webcomponents-loader.js"></script>		
       </head>
       <body {...props.bodyAttributes}>
         <SkipLink mainContent="#content"/>
@@ -22,13 +23,16 @@ export default function HTML(props) {
         <noscript key="noscript" id="gatsby-noscript">
           This app works best with JavaScript enabled.
         </noscript>
+		<script defer src="//www.uoguelph.ca/web-components/UofGHeader-dist.js"></script>
+		<script defer src="//www.uoguelph.ca/web-components/UofGDropdownMenu-dist.js"></script>
+		<script defer src="//www.uoguelph.ca/web-components/UofGIcon-dist.js"></script>
         <Header />
         <div
           key={`body`}
           id="___gatsby"
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
-        {props.postBodyComponents}
+        {props.postBodyComponents}		
         <script defer src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js" crossOrigin="anonymous"></script>
         <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossOrigin="anonymous"></script>
         <script defer type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js" crossOrigin="anonymous"></script>
