71a72b04bd3395bb61e04f2073ccdfe13ce223d7
Scott Schau <sschau@uoguelph.ca>
2021-03-26 11:41:10 -0400

    Button Formatting

--- src/components/sectionButtons.js
+++ src/components/sectionButtons.js
@@ -1,62 +1,47 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import SVG from 'react-inlinesvg';
 import '../styles/button-widget.css'
 import { contentExists } from '../utils/ug-utils';
+import Button from "../components/button";
 
-
-function ButtonStyle (styleOfButton){
-    switch(styleOfButton){
-        case 'Primary':
-            return 'btn-primary';
-        case 'Primary (Outline)':
-            return 'btn-outline-primary';
-        case 'Secondary':
-            return 'btn-secondary';
-        case 'Secondary (Outline)':
-            return 'btn-outline-secondary';
+function ColumnClass (columnName) {
+    switch (columnName){
+        case 'right':
+            return 'btn-block section-button-rigth';
+        case 'left':
+            return 'section-button-left';
         default:
-            return 'btn-primary';
-    }
+            return 'section-button-main';
+    }      
 }
 
-function FontAwesomeIconColour (colourChoice) {
-    switch(colourChoice) {
-        case 'Yellow':
-            return 'yellow-icon';
-        case 'Red':
-            return 'red-icon';
-        case 'Darker Red':
-            return 'red-darker-icon)';
-        default: 
-            return '';
-    }
-}
 
-function ButtonsSection (props) {
+function SectionButtons (props) {
+console.log (props)
 console.log (props.pageData.relationships.field_buttons)
+//
+// Set variables to control button layout 
+//      if column is right use section-button-right
+//      if column is left use section-button-left
+//      if column is main use section-button-main
+//
+// Note: if button has a Font Awesome Icon - the placement of the FA Icon is affected by the column
+// 
+console.log(props.pageData.relationships.field_section_column.name)
+
+const columnClass = (contentExists(props.pageData.relationships.field_section_column.name))? ColumnClass (props.pageData.relationships.field_section_column.name): "section-button-main"
+console.log(columnClass)
 let fieldButtonsData = contentExists(props.pageData.relationships.field_buttons)? props.pageData.relationships.field_buttons: '';
     return (
         <React.Fragment>
             {contentExists(fieldButtonsData) && fieldButtonsData.length !== 0 && <>
                 {fieldButtonsData.map (buttonData => {
                     console.log(buttonData)
-                    let buttonLinkURI = buttonData.field_button_link.uri;
-                    let buttonLinkTitle = contentExists(buttonData.field_formatted_title)? buttonData.field_formatted_title.processed:
-                        contentExists(buttonData.field_button_link.title)? buttonData.field_button_link.title: "no title entered";    
-                                           
-                    // let buttonLinkTitle = contentExists(buttonData.field_button_link)? buttonData.field_button_link.title: "no title entered";    
-                                           
-                    let buttonClassName= 'btn ' + ButtonStyle(buttonData.relationships.field_button_style.name) +' btn-block section-button' ;
-                    let buttonFontAwesomeClassName = contentExists(buttonData.field_font_awesome_icon)? buttonData.field_font_awesome_icon +
-                                                ' section-button-icon fa-fw '+ FontAwesomeIconColour(buttonData.relationships.field_font_awesome_icon_colour.name):'';
-    
-                    return <React.Fragment key={buttonData.drupal_id}>
-                        <a href={buttonLinkURI} className={buttonClassName}>
-                            <i aria-hidden="true" className={buttonFontAwesomeClassName} > </i>
-                            <span className="section-button-text" dangerouslySetInnerHTML={{__html: buttonLinkTitle}} />
-                        </a>
-                    </React.Fragment>
+                    return Button(buttonData,
+                                 columnClass,
+                                 'section-button-icon fa-pull-left',
+                                 'section-button-text',
+                        )
                 })}
            </> }
         </React.Fragment>
@@ -64,11 +49,11 @@ let fieldButtonsData = contentExists(props.pageData.relationships.field_buttons)
 
 }
 
-ButtonsSection.propTypes = {
+SectionButtons.propTypes = {
     pageData: PropTypes.object,
 }
 
-ButtonsSection.defaultProps = {
+SectionButtons.defaultProps = {
     pageData: '',
 }
-export default ButtonsSection
+export default SectionButtons
