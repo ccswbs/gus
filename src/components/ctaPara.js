c81158490826f3d49d60395def2cee1e626cb4df
snschau <sschau@uoguelph.ca>
2021-04-05 15:24:23 -0400

    to revert ctaPara

--- src/components/ctaPara.js
+++ src/components/ctaPara.js
@@ -6,16 +6,14 @@ function ctaPara (props) {
     let buttonClass = 'btn btn-secondary btn-block';
     let faIcon = 'fa-backpack';
     return(
-        <div>
-            {/* <section className="col-md-9 content-area"> */}
-                <div className="">
-                    {/* <h3>{props.pageData.field_cta_title}</h3>
-                    {props.pageData.field_cta_description} <br /> */}
-                    <a href={props.pageData.field_cta_primary_link.uri} className={buttonClass}>
-                        <i aria-hidden="true" className="fas fa-backpack fa-pull-left" style="font-size: 5rem; color:var(--uog-yellow);"></i>
-                        {props.pageData.field_cta_primary_link.title}</a>    					                        
+        <div className="row row-with-vspace site-content">
+            <section className="col-md-9 content-area">
+                <div className="cta border border-dark">
+                    <h3>{props.pageData.field_cta_title}</h3>
+                    {props.pageData.field_cta_description} <br />
+                    <a href={props.pageData.field_cta_primary_link.uri} className="btn btn-uogRed">{props.pageData.field_cta_primary_link.title}</a>    					                        
                 </div>
-             {/* </section> */}
+            </section>
         </div>
     )      
 }
