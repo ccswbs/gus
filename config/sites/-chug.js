7970f7a9177af5d8cc1f2e3e68092380f020c262
snschau <sschau@uoguelph.ca>
2021-08-26 16:08:04 -0400

    update site files for chug (-chug.js)

---
+++ config/sites/-chug.js
@@ -0,0 +1,10 @@
+var metaConfig = {
+    title: "University of Guelph",
+    description: "The University of Guelph, and everyone who studies here, explores here, teaches here and works here is committed to one simple purpose: To Improve Life",
+    author: "@uofg",
+    GAtrackingID: "UA-106745064-4",
+    IGuser: "5851379098",
+    menus: ["main"],
+};
+
+module.exports = metaConfig;
