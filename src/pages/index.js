14a284c347af92f23e01f33c8a75f1d594cc6ea8
Tanya Qureshi <tqureshi@uoguelph.ca>
2021-09-07 16:06:16 -0400

    replace tabs with spaces

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,59 +5,57 @@ import React from 'react';
 import SEO from '../components/seo';
 
 const IndexPage = ({ data }) => (
-        <Layout>
-			<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-			<div className="container page-container">
-				<h1>Gatsby UG Starter Theme</h1>
-				<p>The University of Guelph, and everyone who studies here, explores here, teaches here and works here, is committed to one simple purpose: To Improve Life.</p>
-				<h2>Pages</h2>
-				<ul>
-					{data.allNodePage.edges.map((edge, index) => (
-						<li key={index}><Link to={edge.node.path.alias}>{edge.node.title}</Link></li>
-					))}
-				</ul>
+    <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <div className="container page-container">
+            <h1>Gatsby UG Starter Theme</h1>
+            <p>The University of Guelph, and everyone who studies here, explores here, teaches here and works here, is committed to one simple purpose: To Improve Life.</p>
+            <h2>Pages</h2>
+            <ul>
+                {data.allNodePage.edges.map((edge, index) => (
+                    <li key={index}><Link to={edge.node.path.alias}>{edge.node.title}</Link></li>
+                ))}
+            </ul>
 
-				<h2>Programs</h2>
-				<ul>
-					{data.programs.edges.map((edge, index) => (
-						<li key={index}><Link to={edge.node.path.alias}>{edge.node.title}</Link></li>
-					))}
-				</ul>
-
-				
-				<h2>Instagram</h2>
-				<Instagram />
-			</div>
-        </Layout>
-	)
+            <h2>Programs</h2>
+            <ul>
+                {data.programs.edges.map((edge, index) => (
+                    <li key={index}><Link to={edge.node.path.alias}>{edge.node.title}</Link></li>
+                ))}
+            </ul>
+            
+            <h2>Instagram</h2>
+            <Instagram />
+        </div>
+    </Layout>
+)
 
 export default IndexPage
 
 export const query = graphql`
-	query {
-		allNodePage {
-			edges {
-				node {
-					title
-					drupal_id
-					path {
-						alias
-					}
-				}
-			}
-		}
-		programs: allNodeProgram {
-			edges {
-				node {
-					drupal_id
-					drupal_internal__nid
-					title
-					path {
-						alias
-					}
-				}
-			}
-		}
-		
-	}
+    query {
+      allNodePage {
+        edges {
+          node {
+            title
+            drupal_id
+            path {
+              alias
+            }
+          }
+        }
+      }
+      programs: allNodeProgram {
+        edges {
+          node {
+            drupal_id
+            drupal_internal__nid
+            title
+            path {
+              alias
+            }
+          }
+        }
+      }        
+    }
 `
