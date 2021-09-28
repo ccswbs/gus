51a4d25eeae24a8b71772ab756b5f47b0b63c5ef
Scott Schau <sschau@uoguelph.ca>
2021-09-28 19:18:27 -0400

    fa colour change for stats

--- src/components/stats.js
+++ src/components/stats.js
@@ -16,14 +16,14 @@ function Stats (props) {
 					let statType = stat.relationships.field_stat_type.name;
 					let statValue = stat.field_stat_value;
 					let statValueEnd = stat.field_stat_value_end;
-					let statFAIcon = stat.field_font_awesome_icon;
+					let statFAIcon = stat.field_font_awesome_icon ;
 
 
 					return <React.Fragment key={stat.drupal_id}>
 					<div className="uog-card">
 						<dt>
 							{statIcon !== null && <><SVG src={statIcon.publicURL} /></>} 
-							{<i className={statFAIcon}> </i>}
+							{<span className="fa-icon-colour"><i className={statFAIcon}>  </i></span>}
 							{statRange === true && statValueEnd !== null ? statValue + " - " + statValueEnd : statValue}
 						</dt>
 						<dd>{statType}</dd>
