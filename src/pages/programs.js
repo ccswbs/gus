fe6888c93adddee15b4dfbd60b092f16f814d985
Scott Schau <sschau@uoguelph.ca>
2021-11-02 10:52:42 -0400

    add programs.js page

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -5,17 +5,19 @@ import Seo from '../components/seo';
 
 const IndexPage = ({ data }) => (
     <Layout>
-        <Seo title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      {console.log(data.allNodeProgram)}
+
+      {console.log (data.allNodeProgram)}
+        <Seo title="Program List" keywords={[`gatsby`, `application`, `react`]} />
         <div className="container page-container">
             <h1>Gatsby UG Starter Theme</h1>
             <p>The University of Guelph, and everyone who studies here, explores here, teaches here and works here, is committed to one simple purpose: To Improve Life.</p>
 
             <h2>Programs</h2>
             <ul>
-                {data.programs.edges.map((edge, index) => (
+                {data.allNodeProgram.edges.map((edge, index) => (
                   <div>
                     <li key={index}><Link to={edge.node.path.alias}>{edge.node.title}</Link></li>
-                    {console.log(edge.node)}
                     <span>{edge.node.relationships.field_program_acronym.name}</span>
                   </div>
                 ))}
@@ -28,7 +30,7 @@ export default IndexPage
 
 export const query = graphql`
     query {
-      programs: allNodeProgram {
+      allNodeProgram {
         edges {
           node {
             drupal_id
