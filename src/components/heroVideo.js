a2b15915a8f764371e246189b2c935e1b20c839d
Tanya Qureshi <tqureshi@uoguelph.ca>
2021-11-05 13:08:14 -0400

    Add Pause button

--- src/components/heroVideo.js
+++ src/components/heroVideo.js
@@ -1,46 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import PropTypes from 'prop-types';
 import { contentExists } from '../utils/ug-utils';
 
-function HeroVideo (props) {
-	let playerID = props.playerID;
-	let videoCC = props.videoCC;
-    let videoSize = props.videoSize;
-	let videoTranscript = props.videoTranscript;
-	let videoType = (props.videoURL.includes("youtube") || props.videoURL.includes("youtu.be") ? `youtube` : `vimeo`);	
-	let videoID = (videoType === `youtube` ? props.videoURL.substr(props.videoURL.length - 11) : props.videoURL.substr(18));
+class HeroVideo extends Component {
+    
+    state = {
+        playing: true,
+        muted: true,
+        loop: true
+    }
+    
+    ref = player => {
+        this.player = player
+    }
+    handlePlayPause = () => {
+        this.setState({ playing: !this.state.playing })
+    }
+    
+    render() {
+        let playerID = this.props.playerID;
+        let videoCC = this.props.videoCC;
+        let videoSize = this.props.videoSize;
+        let videoTranscript = this.props.videoTranscript;
+        let videoType = (this.props.videoURL.includes("youtube") || this.props.videoURL.includes("youtu.be") ? `youtube` : `vimeo`);	
+        let videoID = (videoType === `youtube` ? this.props.videoURL.substr(this.props.videoURL.length - 11) : this.props.videoURL.substr(18));
 
-	let youtubeURL = "https://www.youtube.com/embed/";
-	let vimeoURL = "https://player.vimeo.com/video/";	
-	let videoSrc = (videoType === `youtube` ? youtubeURL + videoID : vimeoURL + videoID);
-	
-	return (
-		<React.Fragment>        
-        <div style="max-width:100%; max-height:60rem;" className={"embed-responsive embed-responsive-" + videoSize}>
-        <ReactPlayer url={videoSrc}
-            playing
-            loop
-            muted
-            width="100%"
-            height="100%"
-        />
-        </div>      
-        </React.Fragment>
-	)
+        let youtubeURL = "https://www.youtube.com/embed/";
+        let vimeoURL = "https://player.vimeo.com/video/";	
+        let videoSrc = (videoType === `youtube` ? youtubeURL + videoID : vimeoURL + videoID);
+        
+        const { playing, muted, loop } = this.state;
+        
+        return (
+            <React.Fragment>        
+            <div className={"embed-responsive embed-responsive-" + videoSize}>
+            <button style="width:100px; position:absolute; top:10%; left:50%; transform:translate(-10%,-50%); background:#c20430; color:#fff; font-weight:bold; z-index:50;" onClick={this.handlePlayPause}>{playing ? 'Pause' : 'Play'}</button>
+            <ReactPlayer
+                ref={this.ref}
+                url={videoSrc}
+                playing={playing}
+                loop={loop}
+                muted={muted}
+                width="100%"
+                height="100%"
+            />
+            </div>      
+            </React.Fragment>
+        )
+    }
 }
+
 HeroVideo.propTypes = {
-	playerID: PropTypes.string,
-	videoURL: PropTypes.string,
-	videoTranscript: PropTypes.string,
-	videoCC: PropTypes.string,
+    playerID: PropTypes.string,
+    videoURL: PropTypes.string,
+    videoTranscript: PropTypes.string,
+    videoCC: PropTypes.string,
     videoSize: PropTypes.string,
 }
 HeroVideo.defaultProps = {
-	playerID: ``,
-	videoURL: ``,
-	videoTranscript: ``,
-	videoCC: ``,
+    playerID: ``,
+    videoURL: ``,
+    videoTranscript: ``,
+    videoCC: ``,
     videoSize: `16by9`,
 }
 export default HeroVideo
