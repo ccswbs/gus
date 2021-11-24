1fa01d37a113804950fdb22ef05e08dd3b12f692
Scott Schau <sschau@uoguelph.ca>
2021-11-24 14:05:50 -0500

    Sort Programs

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -4,30 +4,37 @@ import React from 'react';
 import Seo from '../components/seo';
 import '../styles/programsList.css';
 
-const IndexPage = ({ data }) => (
+const ProgramsPage = ({ data }) => (
     <Layout>
-      {console.log(data.allNodeProgram)}
-
-      {console.log (data.allNodeProgram)}
+      {console.log(data.allNodeProgram.edges)}
+      {data.allNodeProgram.edges.sort(function(a,b){
+        var titleA =a.node.title, titleB = b.node.title;
+        if (titleA < titleB)
+          return -1;
+        if (titleA > titleB)
+          return 1;
+        return 0;
+      })}
+      {console.log (data.allNodeProgram.edges)}
         <Seo title="Program List" keywords={[`gatsby`, `application`, `react`]} />
         <div className="container page-container">
             <h1>Programs</h1>
-            <dl className="card-group programsList">
+            <div className="card-group programsList">
                 {data.allNodeProgram.edges.map((edge, index) => (
-                  <dt className="programsList-card p">
+                  <div className="programsList-card p">
                     <h2 key={index}><Link to={edge.node.path.alias}>{edge.node.title}</Link></h2>
                     <dd>{edge.node.relationships.field_program_acronym.name}</dd>
                     {/* <dd>{edge.node.relationships.field_tags.map((tags, indexTags) => (
                       <span key={indexTags}>{tags.name}, </span>
                     ))}</dd> */}
-                  </dt>
+                  </div>
                 ))}
-            </dl>            
+            </div>            
         </div>
     </Layout>
 )
 
-export default IndexPage
+export default ProgramsPage
 
 export const query = graphql`
     query {
