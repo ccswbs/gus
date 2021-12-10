049a27a5bfc55973d8c95789aaf210a921fbe2f2
Tanya Qureshi <tqureshi@uoguelph.ca>
2021-12-10 15:22:37 -0500

    Restore gatsby preact plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-preact`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
