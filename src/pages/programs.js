f9291d3e381ae9268ea4023ad3ef33adda2045c9
Scott Schau <sschau@uoguelph.ca>
2022-01-24 15:03:08 -0500

    clean up code - remove console.log

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -13,13 +13,11 @@ const ProgramsPage = ({ data }) => (
                 <p>The University of Guelph is a comprehensive university offering a wide variety of programs and specialties. From general arts, commerce and science degrees, to unique programs in indigenous environmental science, veterinary technology, bio-resource management and one health. Explore everything your future can be at the University of Guelph.</p>     
                 <br/>
                 <div id="program-list" className="row row-cols-1 row-cols-md-4 g-4 mb-5">
-                  {console.log(data)}
                     {data.allNodeProgram.edges.map((edge, index) => (
                       edge.node.status=== true && <div className="col">
                           <div className="card h-100">
                             <h2 key={index}><a className="stretched-link" href={edge.node.path.alias}>{edge.node.title}</a></h2>
                             <div>{edge.node.relationships.field_program_acronym.name}</div>
-                            <div>status={edge.node.status=== true && <span>true</span>}</div>
                           </div>
                       </div>
                     ))}
