ada43c1f6d1b3b9a6cf300b608d2ca02a2b68221
Scott Schau <sschau@uoguelph.ca>
2022-01-24 14:16:10 -0500

    add program list page text

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -9,13 +9,17 @@ const ProgramsPage = ({ data }) => (
         <Seo title="Program List" description={[`Univeristy of Guelph Programs`]} />
         <div className="container page-container">
             <div id="content" className="site-content">
-                <h1>Programs</h1>            
+                <h1>Programs</h1>       
+                <p>The University of Guelph is a comprehensive university offering a wide variety of programs and specialties. From general arts, commerce and science degrees, to unique programs in indigenous environmental science, veterinary technology, bio-resource management and one health. Explore everything your future can be at the University of Guelph.</p>     
+                <br/>
                 <div id="program-list" className="row row-cols-1 row-cols-md-4 g-4 mb-5">
+                  {console.log(data)}
                     {data.allNodeProgram.edges.map((edge, index) => (
                       <div className="col">
                           <div className="card h-100">
                             <h2 key={index}><a className="stretched-link" href={edge.node.path.alias}>{edge.node.title}</a></h2>
                             <div>{edge.node.relationships.field_program_acronym.name}</div>
+                            <div>status={edge.node.status=== true && <span>true</span>}</div>
                           </div>
                       </div>
                     ))}
@@ -38,6 +42,7 @@ export const query = graphql`
             path {
               alias
             }
+            status
             relationships {
               field_program_acronym {
                 name
