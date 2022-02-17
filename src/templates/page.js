86f0ec455a71edbf7847f26b12f063faba39cb1c
Miranda Mafe <miranda.mafe@gmail.com>
2022-02-16 21:04:12 -0500

    Rearrange imports to trigger PR build again

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
-import { Helmet } from 'react-helmet';
 import Seo from '../components/seo';
+import { Helmet } from 'react-helmet';
 import Hero from '../components/shared/hero'; 
 import Breadcrumbs from '../components/shared/breadcrumbs';
-import CustomFooter from '../components/shared/customFooter';
 import Widget from '../components/shared/widget';
+import CustomFooter from '../components/shared/customFooter';
 import { contentExists } from '../utils/ug-utils';
 
 const Page = ({nodeID, pageTitle, ogDescription, ogImage, ogImageAlt, imageData, widgets, footer, menuName}) => (
