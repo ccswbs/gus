09e9cf29e1b8c291ff627c6c6f3d2edab731f693
Miranda Mafe <miranda.mafe@gmail.com>
2022-02-22 10:09:32 -0500

    Use className instead of class

--- src/components/dateModified.js
+++ src/components/dateModified.js
@@ -6,7 +6,7 @@ function DateModified ({ date }) {
     const lastModified = moment(date).tz('America/Toronto').format(`MMMM D, YYYY HH:mm z`);
     if(date !== null && date !== ""){
         return <div className="container-fluid date-modified">
-                    <div class="container ft-container">
+                    <div className="container ft-container">
                         <strong>Last updated:</strong> {lastModified}
                     </div> 
                </div>
