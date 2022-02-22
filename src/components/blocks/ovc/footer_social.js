c543aa34aa2d5c7fe441ff739d231037fe22e524
Scott Schau <sschau@uoguelph.ca>
2022-02-22 13:17:19 -0500

    hover issue for assistance

--- src/components/blocks/ovc/footer_social.js
+++ src/components/blocks/ovc/footer_social.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../../../styles/ovc/social-icons.css';
 
 // Social icons
 import {
@@ -12,23 +13,23 @@ import {
 const FooterSocial = () => (
   <div>
     <h4 className="mt-4 text-dark">Stay Connected</h4>
-    <a href="https://instagram.com/ontvetcollege/" className="text-dark">
+    <a href="https://instagram.com/ontvetcollege/" className="text-dark-social">
       <span className="sr-only">Connect with OVC on Instagram</span>
       <FaInstagram style={{fontSize: "4.8rem"}}/>
     </a>
-    <a href="https://www.linkedin.com/school/ontario-veterinary-college/" className="text-dark">
+    <a href="https://www.linkedin.com/school/ontario-veterinary-college/" className="text-dark-social">
       <span className="sr-only">Connect with OVC on LinkedIn</span>
       <FaLinkedin style={{fontSize: "4.8rem"}}/>
     </a>
-    <a href="http://www.facebook.com/ontvetcollege" className="text-dark">
+    <a href="http://www.facebook.com/ontvetcollege" className="text-dark-social">
       <span className="sr-only">Connect with OVC on Facebook</span>
       <FaFacebookSquare style={{fontSize: "4.8rem"}} />
     </a>
-    <a href="http://twitter.com/OntVetCollege/" className="text-dark">
+    <a href="http://twitter.com/OntVetCollege/" className="text-dark-social">
       <span className="sr-only">Connect with OVC on Twitter</span>
       <FaTwitterSquare style={{fontSize: "4.8rem"}} />
     </a>
-    <a href="http://www.youtube.com/user/OntarioVetCollege" className="text-dark">
+    <a href="http://www.youtube.com/user/OntarioVetCollege" className="text-dark-social">
       <span className="sr-only">Connect with OVC on YouTube</span>
       <FaYoutubeSquare style={{fontSize: "4.8rem"}} />
     </a>
