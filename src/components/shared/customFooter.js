302a9b8b3c187d628611a5a1f6c1262919d16c90
Miranda Mafe <miranda.mafe@gmail.com>
2022-02-28 12:51:59 -0500

    Add footerOVC component

--- src/components/shared/customFooter.js
+++ src/components/shared/customFooter.js
@@ -1,45 +1,39 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import FooterSocial from './blocks/ovc/footer_social';
 import { graphql } from 'gatsby';
 import { GatsbyImage } from "gatsby-plugin-image";
+import OVCCustomFooter from 'components/blocks/ovc/footerOVC';
 import Widget from 'components/shared/widget';
 import { contentExists } from 'utils/ug-utils';
 import 'styles/customFooter.css';
 
-
 function CustomFooter (props) {
+	
 	const footer = props.footerData;
-  // check if footer is from OVC - set conditions so layout is sligtly different as well as include OVC social media
-  var footerOVCCust = "no";
-  var firstClassCol = "col-md-3 content-area";
-  var secondClassCol = "col-md-9 content-area";
-  footer.node.relationships.field_tags.forEach(element => {if (element.__typename === "taxonomy_term__units" && element.name.includes("Ontario Veterinary College"))
-      footerOVCCust= "yes"
-    });
-  if (footerOVCCust === "yes") {
-    firstClassCol = "col-md-4 content-area";
-    secondClassCol = "col-md-8 content-area";
-  }
 	const footerLogos = (contentExists(footer.node.relationships.field_footer_logo) ? footer.node.relationships.field_footer_logo : null);
 	const footerText = (contentExists(footer.node.body.processed) ? footer.node.body.processed : null);
 	const footerWidgets = (contentExists(footer.node.relationships.field_widgets) ? footer.node.relationships.field_widgets : null);
+
+  // OVC custom footer
+  footer.node.relationships.field_tags.forEach(
+    element => {if (element.__typename === "taxonomy_term__units" && element.name.includes("Ontario Veterinary College")){
+      return <OVCCustomFooter footerData={props} />
+    }});
 		
 	return <>	
 		<div className="full-width-container bg-light pre-footer">
 			<div className="container page-container">
 				<section className="row row-with-vspace site-content">
 					{contentExists(footerLogos) &&
-					<div className={firstClassCol}>
+					<div className="col-md-3 content-area">
 					{footerLogos.map(logo => (
 						<GatsbyImage
                             image={logo.relationships.field_media_image.localFile.childImageSharp.gatsbyImageData}
                             className="footer-logo"
                             alt={logo.field_media_image.alt} />
 					))}
-         {footerOVCCust === "yes" && FooterSocial()}
 					</div>}
-					<div className={secondClassCol}>
+					<div className="col-md-9 content-area">
 						<div className="container" dangerouslySetInnerHTML={{ __html: footerText}} />
 						<Widget pageData={footerWidgets} />
 					</div>
@@ -76,7 +70,6 @@ export const query = graphql`
                   name
                 }
               }
-
               field_footer_logo {
                 field_media_image {
                   alt
@@ -97,5 +90,4 @@ export const query = graphql`
               }
             }
 	}
-
 `
