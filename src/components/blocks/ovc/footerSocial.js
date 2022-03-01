62ea85f7f502b0497068530a6a61aa5177bd5062
Scott Schau <sschau@uoguelph.ca>
2022-03-01 09:28:59 -0500

    http to https and sr-only to visually-hidden

--- src/components/blocks/ovc/footerSocial.js
+++ src/components/blocks/ovc/footerSocial.js
@@ -14,23 +14,23 @@ const FooterSocial = () => (
   <div>
     <h4 className="mt-4 text-dark">Stay Connected</h4>
     <a href="https://instagram.com/ontvetcollege/" className="text-dark-social">
-      <span className="sr-only">Connect with OVC on Instagram</span>
+      <span className="visually-hidden">Connect with OVC on Instagram</span>
       <FaInstagram style={{fontSize: "4.8rem"}}/>
     </a>
     <a href="https://www.linkedin.com/school/ontario-veterinary-college/" className="text-dark-social">
-      <span className="sr-only">Connect with OVC on LinkedIn</span>
+      <span className="visually-hidden">Connect with OVC on LinkedIn</span>
       <FaLinkedin style={{fontSize: "4.8rem"}}/>
     </a>
-    <a href="http://www.facebook.com/ontvetcollege" className="text-dark-social">
-      <span className="sr-only">Connect with OVC on Facebook</span>
+    <a href="https://www.facebook.com/ontvetcollege" className="text-dark-social">
+      <span className="visually-hidden">Connect with OVC on Facebook</span>
       <FaFacebookSquare style={{fontSize: "4.8rem"}} />
     </a>
-    <a href="http://twitter.com/OntVetCollege/" className="text-dark-social">
-      <span className="sr-only">Connect with OVC on Twitter</span>
+    <a href="https://twitter.com/OntVetCollege/" className="text-dark-social">
+      <span className="visually-hidden">Connect with OVC on Twitter</span>
       <FaTwitterSquare style={{fontSize: "4.8rem"}} />
     </a>
-    <a href="http://www.youtube.com/user/OntarioVetCollege" className="text-dark-social">
-      <span className="sr-only">Connect with OVC on YouTube</span>
+    <a href="https://www.youtube.com/user/OntarioVetCollege" className="text-dark-social">
+      <span className="visually-hidden">Connect with OVC on YouTube</span>
       <FaYoutubeSquare style={{fontSize: "4.8rem"}} />
     </a>
   </div>
