8e6a4b605c7a47de718030e2091c1b6eaf11681a
Tanya Qureshi <tqureshi@uoguelph.ca>
2022-03-08 15:14:42 -0500

    Use Bootstrap 5 accordion code

--- src/components/shared/accordionWidget.js
+++ src/components/shared/accordionWidget.js
@@ -1,28 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { graphql } from 'gatsby';
+import { contentExists } from 'utils/ug-utils';
 
-
-function accordionWidget (props) {
-  
-  return(
-    props.pageData.relationships.field_accordion_block_elements.map((accordionData,j) => {
-        return( <>
-            <div className="panel-group panel-group-lists collapse in show" id={"accordionWidget"+props.pageData.drupal_id}>
-                <div className="panel">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a data-toggle="collapse" data-parent={"#accordionWidget"+props.pageData.drupal_id} href={"#collapse"+j+props.pageData.drupal_id} className="collapsed" dangerouslySetInnerHTML={{__html: accordionData.field_accordion_block_title.processed}}></a>
-                        </h4>
-                    </div>
-                    <div id={"collapse"+j+props.pageData.drupal_id} className="panel-collapse collapse in">
-                        <div className="panel-body" dangerouslySetInnerHTML={{__html: accordionData.field_accordion_block_text.processed}}/>
+const accordionWidget = (props) => {
+    let accordionData = props.pageData.relationships.field_accordion_block_elements;    
+    if (contentExists(accordionData)) {
+        return <>
+            <div className="accordion" id={"accordion" + props.pageData.drupal_id}>            
+            {accordionData.map(item =>
+                <div className="accordion-item">
+                    <h3 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#part" + item.drupal_id} aria-expanded="true" aria-controls="collapseOne" dangerouslySetInnerHTML={{__html: item.field_accordion_block_title.processed}}>
+                        </button>
+                    </h3>
+                    <div id={"part" + item.drupal_id} className="accordion-collapse collapse" aria-labelledby={item.drupal_id} data-bs-parent={"#accordion" + props.pageData.drupal_id}>
+                        <div className="accordion-body" dangerouslySetInnerHTML={{__html: item.field_accordion_block_text.processed}} />
                     </div>
                 </div>
+            )}
             </div>
-        </>);
-    } )
-);
+        </>
+    }
+    return null;
 }
 
 accordionWidget.propTypes = {
@@ -36,29 +36,29 @@ accordionWidget.defaultProps = {
 export default accordionWidget
 
 export const query = graphql`
-fragment AccordionSectionParagraphFragment on paragraph__accordion_section {
-  drupal_id
-  relationships {
-    field_accordion_block_elements {
-      drupal_id
-      field_accordion_block_title {
-        processed
-      }
-      field_accordion_block_text {
-        processed
+  fragment AccordionSectionParagraphFragment on paragraph__accordion_section {
+    drupal_id
+    relationships {
+      field_accordion_block_elements {
+        drupal_id
+        field_accordion_block_title {
+          processed
+        }
+        field_accordion_block_text {
+          processed
+        }
       }
     }
   }
-}
-fragment GeneralTextParagraphFragment on paragraph__general_text {
-  drupal_id
-  field_general_text {
-    processed
-  }
-  relationships {
-    field_section_column {
-      name
+  fragment GeneralTextParagraphFragment on paragraph__general_text {
+    drupal_id
+    field_general_text {
+      processed
+    }
+    relationships {
+      field_section_column {
+        name
+      }
     }
   }
-} 
 `
