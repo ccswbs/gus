ca897964c20d17b9e8416e011a3284ddd0f0127d
Miranda Mafe <miranda.mafe@gmail.com>
2022-03-09 14:06:02 -0500

    Update economic impact aside

--- src/components/blocks/economic-impact/national-impact.js
+++ src/components/blocks/economic-impact/national-impact.js
@@ -16,10 +16,10 @@ const QuoteText = styled.p`
 `
 
 const NationalImpactAside = ({aside}) => (
-    <div className="card bg-dark text-white p-5 rounded-0 align-self-center me-5">
-        <div className="card-body px-2">
+    <div className="card bg-dark border-0 text-white p-5 rounded-0 align-self-center me-5">
+        <div className="card-body px-5">
             <h3 className="mt-0 text-white h1">{aside.title}</h3>
-            <div className="card-text fs-2">
+            <div className="card-text fs-3">
                 {aside.body.map((paragraph, index) => <p key={`intro-text-${index}`}>{paragraph}</p>)}
             </div>
         </div>
@@ -47,17 +47,17 @@ const NationalImpactTestimony = ({ testimonial }) => (
 
 const render = ({ title, lead, body_html, testimonial, aside }) => (
     <>
-        <Container fluid={true}>
-            <div className="full-width-container bg-light">
-                <Container className="page-container">
-                    <Row className="gx-0 site-content mx-4">
-                        <Col md={6} className="pe-4">
+        <Container fluid={true} className="bg-light">
+            <div className="full-width-container">
+                <Container className="page-container pe-0">
+                    <Row className="site-content mx-4 py-0 pe-0">
+                        <Col md={6} className="pe-5 pt-4">
                             <h2 id="national-impact">{title}</h2>
                             <Lead className="text-uppercase"><strong>{lead}</strong></Lead>
                             <div dangerouslySetInnerHTML={{__html: body_html}}></div>
                             <NationalImpactTestimony testimonial={testimonial} />
                         </Col>
-                        <Col md={6} className="d-flex position-relative overflow-hidden">
+                        <Col md={6} className="d-flex position-relative p-0">
                             <GatsbyImage image={getImage(aside.image.src)} alt={aside.image.alt} className="position-absolute top-0 end-0" />
                             <NationalImpactAside aside={aside} />    
                         </Col>
