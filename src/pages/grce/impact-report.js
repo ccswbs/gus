691abd91db6e8c9134dc7ac8d554cd8afc0a6019
Miranda Mafe <miranda.mafe@gmail.com>
2022-03-09 20:49:58 -0500

    Removed commented out code from econimpact

--- src/pages/grce/impact-report.js
+++ src/pages/grce/impact-report.js
@@ -24,68 +24,9 @@ const render = (title) => (
     </Layout>
 )
 
-// export const query = graphql`
-//   query EconomicImpactQuery {
-//     nodePage(drupal_internal__nid: {eq: FILL-IN-ID}) {
-//         drupal_id
-//         drupal_internal__nid
-//         title
-//         field_metatags {
-//             og_description
-//         }
-//         path {
-//             alias
-//         }
-//         relationships {
-//             field_widgets {
-//                 ...FieldWidgetsFragment
-//             }
-
-//             field_tags {
-//                 __typename
-//                 ... on TaxonomyInterface {
-//                     name
-//                 }
-//             }
-//         }
-//     }
-
-//     images: allMediaImage(filter: {relationships: {node__page: {elemMatch: {drupal_internal__nid: {eq: FILL-IN-ID}}}}}) {
-//         edges {
-//             node {
-//                 drupal_id
-//                 ...HeroImageFragment
-//             }
-//         }
-//     }
-
-//     menu: menuLinkContentMenuLinkContent(link: {uri: {eq: "FILL-IN-ID"}}) {
-//         link {
-//             uri
-//             url
-//         }
-//         drupal_parent_menu_item
-//         drupal_id
-//         menu_name
-//     }
-// }
-// `
-
 const EconomicImpactPage = () => (
     render("Economic Impact Report")
 )
-
-// const EconomicImpactPage = ({data}) => (
-//     <EconomicImpactPageRender nodeID={data.nodePage.drupal_internal__nid}
-//           pageTitle={data.nodePage.title} 
-//           ogDescription={data.nodePage.field_metatags?.og_description}
-//           ogImage={data.images.edges[0]?.node.relationships.field_media_image.localFile.publicURL}
-//           ogImageAlt={data.images.edges[0]?.node?.field_media_image.alt}
-//           imageData={data.images.edges}
-//           widgets={data.nodePage.relationships.field_widgets}
-//           menuName={data.menu?.menu_name || `main`}
-//     />
-// )
 
 export default EconomicImpactPage;
 
