3d3f117d7b86fcd8001f218cd38d3d089d9413d3
Tanya Qureshi <tqureshi@uoguelph.ca>
2022-03-31 15:30:54 -0400

    Refactor Employers component for Bootstrap 5

--- src/components/shared/employers.js
+++ src/components/shared/employers.js
@@ -1,39 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { GatsbyImage } from "gatsby-plugin-image";
-import 'styles/employers.css';
 
 function Employers (props) {
-    return (
-        <div className="container">
-                <div className="row">
-                    {props.employerData.map (unit => {
-                        let employerImage = unit.node.relationships.field_image;
-                        let employerImageAlt = unit.node.field_image_alt ? unit.node.field_image_alt : ``;
-                        let employerSummary = unit.node.field_employer_summary;
-                        let employerJobPostingsLink = unit.node.field_link ? unit.node.field_link.uri : null;
-                        return (
-                            <div className="col-6 col-md-4" key={unit.node.drupal_id}>
-                                    <div className="employer-wrapper">
-                                    {employerImage && employerImage.localFile && <div className="employer-pic">
-                                        <GatsbyImage
-                                            image={employerImage.localFile.childImageSharp.gatsbyImageData}
-                                            imgStyle={{ objectFit: 'contain' }}
-                                            alt={employerImageAlt} />
-                                    </div>}
-                                    <div className="employer-info">
-                                        <h4 className="employer-name">{unit.node.title}</h4>
-                                        {employerSummary && <div dangerouslySetInnerHTML={{__html: employerSummary.processed}} />}
-                                        {employerJobPostingsLink && <p><a href={unit.node.field_link.uri}>Current Job Postings<span className="sr-only"> for {unit.node.title}</span></a></p>}
-                                    </div>
-                                    </div>
-                                </div>
-                        );
-                        })}
+    return (<>
+    <ul className="no-icon row">
+        {props.employerData.map (employer => {
+            let employerImage = employer.node.relationships.field_image;
+            let employerImageAlt = employer.node.field_image_alt ? employer.node.field_image_alt : ``;
+            let employerSummary = employer.node.field_employer_summary;
+            let employerJobPostingsLink = employer.node.field_link?.uri;
+            return (<>                
+                <li className="card border-0 col-md-6 col-lg-4 flex-row">
+                    {employerImage && employerImage.localFile && 
+                    <div className="col-2 col-md-3 p-1 p-md-3">
+                        <GatsbyImage
+                            image={employerImage.localFile.childImageSharp.gatsbyImageData}
+                            imgStyle={{ objectFit: 'contain' }}
+                            alt={employerImageAlt} />
+                    </div>}
+                    <div className="card-body col-11 col-md-9">
+                        <p><strong>{employer.node.title}</strong></p>
+                        {employerSummary && <div dangerouslySetInnerHTML={{__html: employerSummary.processed}} />}
+                        {employerJobPostingsLink && <a href={employer.node.field_link.uri}>Current Job Postings<span className="visually-hidden"> for {employer.node.title}</span></a>}
+                    
                     </div>
-                </div>
-    );
+                </li>
+            </>);
+        })}
+    </ul></>)
 }
+
 
 Employers.propTypes = {
 	employerData: PropTypes.array,
