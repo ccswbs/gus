4a0b536e4b72bdfa5e0aea5c9e066a73c83ae755
Tanya Qureshi <tqureshi@uoguelph.ca>
2022-04-04 11:21:44 -0400

    Use getImage function

--- src/components/shared/employers.js
+++ src/components/shared/employers.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { GatsbyImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 function Employers (props) {
     return (<>
     <ul className="no-icon row">
         {props.employerData.map (employer => {
-            let employerImage = employer.node.relationships.field_image;
+            let employerImage = getImage(employer.node.relationships.field_image?.localFile);
             let employerImageAlt = employer.node.field_image_alt ? employer.node.field_image_alt : ``;
             let employerSummary = employer.node.field_employer_summary;
             let employerJobPostingsLink = employer.node.field_link?.uri;
             return (                
                 <li key={employer.node.drupal_id} className="card border-0 col-md-6 col-lg-4 flex-row">
-                    {employerImage && employerImage.localFile && 
+                    {employerImage && 
                     <div className="col-2 col-md-3 p-1 p-md-3">
                         <GatsbyImage
-                            image={employerImage.localFile.childImageSharp.gatsbyImageData}
+                            image={employerImage}
                             imgStyle={{ objectFit: 'contain' }}
                             alt={employerImageAlt} />
                     </div>}
                     <div className="card-body col-11 col-md-9">
                         <p><strong>{employer.node.title}</strong></p>
                         {employerSummary && <div dangerouslySetInnerHTML={{__html: employerSummary.processed}} />}
-                        {employerJobPostingsLink && <a href={employer.node.field_link.uri}>Current Job Postings<span className="visually-hidden"> for {employer.node.title}</span></a>}
-                    
+                        {employerJobPostingsLink && <a href={employerJobPostingsLink}>Current Job Postings<span className="visually-hidden"> for {employer.node.title}</span></a>}                    
                     </div>
                 </li>);
         })}
