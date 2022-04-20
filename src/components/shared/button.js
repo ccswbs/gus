e16260f4ddf933fc6d5a29a67f384ebe16bab5bb
Tanya Qureshi <tqureshi@uoguelph.ca>
2022-04-20 19:24:42 -0400

    Adjust display classes

--- src/components/shared/button.js
+++ src/components/shared/button.js
@@ -50,10 +50,11 @@ function Button (buttonData, buttonWidth) {
     let buttonLinkTitle = buttonData?.field_formatted_title ? buttonData.field_formatted_title.processed : (buttonData.field_button_link?.title ? buttonData.field_button_link.title : "No title entered");
     let buttonIcon = buttonData?.field_font_awesome_icon;
     let buttonIconColour = buttonData.relationships.field_font_awesome_icon_colour?.name;
-    let buttonClasses = "btn " + buttonStyle(buttonData.relationships.field_button_style?.name) + " " + buttonWidth + " no-icon p-4 text-start";
-    let buttonFontAwesomeClasses = "align-middle col-3 d-table-cell display-1 " + buttonIcon + (buttonIconColour ? " " + fontAwesomeIconColour(buttonIconColour) : null);
+    let buttonClasses = "btn " + buttonStyle(buttonData.relationships.field_button_style?.name) + " " + buttonWidth + " no-icon p-4 text-start d-table-row";
+    let buttonFontAwesomeClasses = "align-middle d-table-cell display-1 w-25 " + buttonIcon + (buttonIconColour ? " " + fontAwesomeIconColour(buttonIconColour) : null);
     let btnAnalyticsGoal = buttonData.relationships.field_cta_analytics_goal?.name;
     let btnAnalyticsAction = buttonData.relationships.field_cta_analytics_goal?.field_goal_action;
+    let buttonTitleClasses = "d-table-cell lh-sm";
     
     return (
     <React.Fragment key={buttonData.drupal_id}>
@@ -61,23 +62,23 @@ function Button (buttonData, buttonWidth) {
     {buttonData.field_button_link.uri.includes("http") ? btnAnalyticsGoal && btnAnalyticsAction ? 
         <a href={urlLink} className={buttonClasses} onClick={e => {trackCustomEvent({category: btnAnalyticsGoal,action: btnAnalyticsAction,})}}>		
             {buttonIcon && <i aria-hidden="true" className={buttonFontAwesomeClasses}> </i>}
-            <span className="d-table-cell lh-sm" dangerouslySetInnerHTML={{__html: buttonLinkTitle}} />
+            <span className={buttonTitleClasses} dangerouslySetInnerHTML={{__html: buttonLinkTitle}} />
         </a>
         :
         <a href={urlLink} className={buttonClasses}>
             {buttonIcon && <i aria-hidden="true" className={buttonFontAwesomeClasses}> </i>}
-            <span className="d-table-cell lh-sm" dangerouslySetInnerHTML={{__html: buttonLinkTitle}} />
+            <span className={buttonTitleClasses} dangerouslySetInnerHTML={{__html: buttonLinkTitle}} />
         </a>
         :
         btnAnalyticsGoal && btnAnalyticsAction ? 
         <Link to={urlLink} className={buttonClasses} onClick={e => {trackCustomEvent({category: btnAnalyticsGoal,action: btnAnalyticsAction,})}}>		
             {buttonIcon && <i aria-hidden="true" className={buttonFontAwesomeClasses}> </i>}
-            <span className="d-table-cell lh-sm" dangerouslySetInnerHTML={{__html: buttonLinkTitle}} />
+            <span className={buttonTitleClasses} dangerouslySetInnerHTML={{__html: buttonLinkTitle}} />
         </Link>
         :
         <Link to={urlLink} className={buttonClasses}>
             {buttonIcon && <i aria-hidden="true" className={buttonFontAwesomeClasses}> </i>}
-            <span className="d-table-cell lh-sm" dangerouslySetInnerHTML={{__html: buttonLinkTitle}} />
+            <span className={buttonTitleClasses} dangerouslySetInnerHTML={{__html: buttonLinkTitle}} />
         </Link>        
     }
     </React.Fragment>)
