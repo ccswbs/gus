a89abe79d0533d7ae12f4d982b87a60b73aba6c3
tqureshi-uog <tqureshi@uoguelph.ca>
2022-05-06 16:34:04 -0400

    Add extra null checks
    Test to see if this resolves Gatsby Cloud build errors

--- src/components/shared/breadcrumbs.js
+++ src/components/shared/breadcrumbs.js
@@ -77,11 +77,11 @@ const makeBreadcrumbTrail = (menuData, domains, menuName, nodeID, nodeTitle) => 
                 ***/
                 if (domains && domains.length > 0 && menuName !== "main") {                    
                     if (domains.length === 1 && domains[0].drupal_internal__target_id.includes("liveugconthub")) {
-                        topCrumb = rootItems[0].node.title;
-                        topCrumbID = rootItems[0].node.link.uri;
-                        topCrumbURL = rootItems[0].node.link.url;
+                        topCrumb = rootItems[0]?.node.title;
+                        topCrumbID = rootItems[0]?.node.link.uri;
+                        topCrumbURL = rootItems[0]?.node.link.url;
                     } else {
-                        homeCrumbURL = rootItems[0].node.link.url;
+                        homeCrumbURL = rootItems[0]?.node.link.url;
                     }                    
                 }
             }            
