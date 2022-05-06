9369c92d2b4b3dfbeb912cb5a0c0ebfebf135b3e
Rob Pinciuc <rob@cheekymonkeymedia.ca>
2022-05-06 12:58:08 -0700

    Remove unnecessary gatsby-browser.js

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-const React = require("react")
-const NiceModal = require("@ebay/nice-modal-react")
-
-exports.wrapRootElement = ({ element }) => {
-  return <NiceModal.Provider>{element}</NiceModal.Provider>
-}
