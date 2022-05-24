74d9857dd8a3031c8d3b13cc6a22234ee35d996a
Tanya Qureshi <tqureshi@uoguelph.ca>
2022-05-24 15:00:12 -0400

    Restore file downloads

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
           username: process.env.BASIC_AUTH_USERNAME,
           password: process.env.BASIC_AUTH_PASSWORD,
         },
-        skipFileDownloads: true,
+        //skipFileDownloads: true,
       },
     },
   
