77d2ed86d8595963f6143a68fade6f4077a013b3
Miranda Mafe <miranda.mafe@gmail.com>
2022-05-24 11:50:09 -0400

    Update content-area styles for impact report

--- src/components/blocks/economic-impact/provincial-impact-stats.js
+++ src/components/blocks/economic-impact/provincial-impact-stats.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import { Col, Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Statistic from 'components/shared/statistic';
 
 const colourOptions = [
@@ -11,20 +11,22 @@ const colourOptions = [
 
 const render = ({ title, stats }) => (
   <Container>
-    <h3 className="visually-hidden">{title}</h3>
-    <Statistic className="row row-cols-1 row-cols-md-3 mb-5 g-4">
-      {stats.map(({value, type, link}, index) => 
-        <Col key={`prov-stat-${index}`} className="col-lg">
-          <Statistic.BorderCard key={`prov-stat-${index}`} border={colourOptions[index].border} >
-            <Statistic.Value><strong>{value}</strong></Statistic.Value>
-            {link ? 
-              <Statistic.Type><a href={link.url} className="fs-3">{type}</a></Statistic.Type>
-              : <Statistic.Type>{type}</Statistic.Type>
-            }
-          </Statistic.BorderCard>
-        </Col>
-      )}
-      </Statistic>
+    <Row className="content-area">
+      <h3 className="visually-hidden">{title}</h3>
+      <Statistic className="row row-cols-1 row-cols-md-3 mb-5 g-4">
+        {stats.map(({value, type, link}, index) => 
+          <Col key={`prov-stat-${index}`} className="col-lg">
+            <Statistic.BorderCard key={`prov-stat-${index}`} border={colourOptions[index].border} >
+              <Statistic.Value><strong>{value}</strong></Statistic.Value>
+              {link ? 
+                <Statistic.Type><a href={link.url} className="fs-3">{type}</a></Statistic.Type>
+                : <Statistic.Type>{type}</Statistic.Type>
+              }
+            </Statistic.BorderCard>
+          </Col>
+        )}
+        </Statistic>
+      </Row>
     </Container>
 )
 
