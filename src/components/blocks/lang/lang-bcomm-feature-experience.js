167c5c8fd59032de15db409467ebb49c3882d956
Miranda Mafe <miranda.mafe@gmail.com>
2022-06-16 16:52:53 -0400

    Update imports

--- src/components/blocks/lang/lang-bcomm-feature-experience.js
+++ src/components/blocks/lang/lang-bcomm-feature-experience.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import { Container, Col } from "react-bootstrap"
+import Overlay from "components/shared/overlay"
 import styled from 'styled-components'
 
 const yaml = require('js-yaml');
