cdd73a25526fc3251c9eb14030c3d25305cf5558
Miranda Mafe <miranda.mafe@gmail.com>
2022-06-24 16:09:29 -0400

    Add Yaml Block example

---
+++ src/components/blocks/yaml-block-example.js
@@ -0,0 +1,127 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import PageContainer from 'components/shared/pageContainer'
+import ModalVideo from "components/shared/modalVideo"
+
+const yaml = require('js-yaml');
+
+const render = ({ field_yaml_map, relationships }) => {
+    let yamlMap;
+    let yamlFiles = {};
+
+    relationships.field_yaml_files.forEach(file => {
+      if( file.path?.alias) {
+        yamlFiles[file.path.alias] = file;
+      }
+    });
+
+    try {
+      yamlMap = yaml.load(field_yaml_map);
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+
+    const fileURL = yamlFiles[yamlMap.file.alias]?.relationships.field_media_file.localFile.publicURL;
+    const imageURL = yamlFiles[yamlMap.image.src]?.relationships.field_media_image.localFile;
+    const video = yamlFiles[yamlMap.video.alias];
+
+    return (
+      <PageContainer.SiteContent>
+        <PageContainer.ContentArea>
+          <h2>{yamlMap.title}</h2>
+          <p>yamlMap.body</p>
+          {yamlMap.body_paragraphs.map((paragraph, index) => <p key={`example-text-${index}`}>{paragraph}</p>)}
+          <div dangerouslySetInnerHTML={{__html: yamlMap.body_html}} />
+          {fileURL && <a href={fileURL}>{yamlMap.file.title}</a>}
+          {imageURL && <GatsbyImage image={getImage(imageURL)} alt={yamlMap.image.alt} />}
+          <p>{video.name}</p>
+          {video && <ModalVideo 
+            id={video.id} 
+            src={video.field_media_oembed_video} 
+            title={video.name} 
+            transcript={video.relationships.field_media_file?.localFile.publicURL} 
+            modalButton = {
+              <button type="button" className="btn btn-primary my-4">
+                  <i className="fa-solid fa-play"></i> Watch Video<span className="visually-hidden">: {video.name}</span>
+              </button>
+            }
+          />}
+        </PageContainer.ContentArea>
+      </PageContainer.SiteContent>
+)}
+
+
+const query = graphql`
+  query {
+    blockContentYamlBlock(field_yaml_id: {glob: "yaml_block_example"}) {
+      id
+      field_yaml_id
+      field_yaml_map
+      relationships {
+        field_yaml_files {
+          __typename
+          ... on media__image {
+            id
+            name
+            relationships {
+              field_media_image {
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+                  }
+                }
+              }
+            }
+            path {
+              alias
+            }
+          }
+          ... on media__file {
+            id
+            drupal_id
+            name
+            relationships {
+              field_media_file {
+                localFile {
+                  publicURL
+                }
+              }
+            }
+            path {
+              alias
+            }
+          }
+          ... on media__remote_video {
+            id
+            drupal_id
+            name
+            field_media_oembed_video
+            field_video_height
+            field_video_width
+            relationships {
+              field_media_file {
+                localFile {
+                  publicURL
+                }
+              }
+              field_video_cc {
+                localFile {
+                  publicURL
+                }
+              }
+            }
+            path {
+              alias
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default function YamlBlockExample() {
+  return <StaticQuery query={query} render={({blockContentYamlBlock}) => render(blockContentYamlBlock)} />
+}
--- src/components/shared/yamlWidget.js
+++ src/components/shared/yamlWidget.js
@@ -3,18 +3,20 @@ import { graphql } from 'gatsby';
 
 // @todo - add index of components in components/blocks and import all in one line
 // until then, import each component manually below
-import InternationalStatsGlobal from 'components/blocks/international/international-stats-global'
-import InternationalExploreThingsToKnow from 'components/blocks/international/international-things-to-know'
-import InternationalExploreButtons from 'components/blocks/international/international-explore-btns'
-import InternationalExploreGrid from 'components/blocks/international/international-explore-grid'
-import InternationalTalkCurrentStudent from 'components/blocks/international/international-talk-current-student'
-import InternationalExploreLead from 'components/blocks/international/international-explore-lead'
+import YamlBlockExample from 'components/blocks/yaml-block-example';
+import InternationalStatsGlobal from 'components/blocks/international/international-stats-global';
+import InternationalExploreThingsToKnow from 'components/blocks/international/international-things-to-know';
+import InternationalExploreButtons from 'components/blocks/international/international-explore-btns';
+import InternationalExploreGrid from 'components/blocks/international/international-explore-grid';
+import InternationalTalkCurrentStudent from 'components/blocks/international/international-talk-current-student';
+import InternationalExploreLead from 'components/blocks/international/international-explore-lead';
 
 const YamlWidget = (props) => {
     let component = props.blockData.relationships.field_custom_block?.field_yaml_id;
 
     // add new custom components to conditional rendering below
     return ({
+        'yaml_block_example': <YamlBlockExample />,
         'international_stats_global_impact': <InternationalStatsGlobal />,
         'international_explore_things_to_know': <InternationalExploreThingsToKnow />,
         'international_explore_btns': <InternationalExploreButtons />,
