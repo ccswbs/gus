80a5b7bbdc9b4c2209cd0e8d453b852d5b862882
Tanya Qureshi <tqureshi@uoguelph.ca>
2022-07-13 14:19:58 -0400

    Restore master versions

--- gatsby-node.js
+++ gatsby-node.js
@@ -66,7 +66,6 @@ field_name: [taxonomy_term__vocabulary_name] @link(from: "field_name___NODE")
 const path = require(`path`)
 const fs = require('fs');
 const yaml = require('js-yaml');
-const util = require('util');
 
 exports.createSchemaCustomization = ({ actions, schema }) => {
 
@@ -121,14 +120,12 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
       | paragraph__modal_video_widget
 
     union widgetSectionParagraphUnion =
-      paragraph__accordion_section
-      | paragraph__block_widget
+      paragraph__block_widget
       | paragraph__general_text
       | paragraph__lead_paragraph
       | paragraph__link_item
       | paragraph__links_widget
       | paragraph__media_text
-      | paragraph__section_tabs
       | paragraph__stats_widget
       | paragraph__section_buttons
       | paragraph__button_widget
@@ -446,7 +443,6 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
     }
     type paragraph__accordion_sectionRelationships implements Node {
       field_accordion_block_elements: [paragraph__accordion_block] @link(from: "field_accordion_block_elements___NODE")
-      field_section_column: taxonomy_term__section_columns @link(from: "field_section_column___NODE")
     }
     type paragraph__block_widget implements Node {
       drupal_id: String
@@ -493,7 +489,6 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
     type paragraph__general_text implements Node {
       drupal_id: String
       field_general_text: BodyField
-      relationships: paragraph__general_textRelationships
     }
     type paragraph__general_textRelationships {
       field_section_column: taxonomy_term__section_columns @link(from: "field_section_column___NODE")
@@ -534,7 +529,6 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
       field_link_image: media__image @link(from: "field_link_image___NODE")
     }
     type paragraph__media_text implements Node {
-      drupal_id: String
       field_media_image_size: String
       field_media_text_desc: BodyField
       field_media_text_title: String
@@ -598,7 +592,6 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
     }
     type paragraph__section_tabsRelationships {
       field_tabs: [paragraph__tab_content] @link(from:"field_tabs___NODE")
-      field_section_column: taxonomy_term__section_columns @link(from: "field_section_column___NODE")
     }
     type paragraph__stats_widget implements Node {
       drupal_id: String
