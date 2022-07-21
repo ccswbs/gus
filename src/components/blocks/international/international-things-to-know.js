20a9f8b858d7f462fbf7cf57f9b5ea8a8d6bbc57
David Wood <dwood@uoguelph.ca>
2022-07-21 11:56:52 -0400

    international-things-to-know logic3

--- src/components/blocks/international/international-things-to-know.js
+++ src/components/blocks/international/international-things-to-know.js
@@ -42,7 +42,8 @@ const render = ({ field_yaml_map, relationships }) => {
                               <p>{text}</p>
                               <div className="d-grid d-md-block gap-2">
                                   {links.map(({title, url}, index) => 
-                                      {$url !== undefined && <a key={`international-explore-btns-${index}`} href ={url} className="btn btn-info me-md-3 no-icon p-4 text-start">{title}</a>}
+                                    <>{$index}</>
+                                    //$url !== undefined && <a key={`international-explore-btns-${index}`} href ={url} className="btn btn-info me-md-3 no-icon p-4 text-start">{title}</a>
                                   )}
                               </div>
                           </MediaCardBody>
