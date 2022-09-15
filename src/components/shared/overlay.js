0b05437f4b374d68f2a961af3a5b9351adc28f7f
Miranda Mafe <miranda.mafe@gmail.com>
2022-09-15 16:45:23 -0400

    Add getImage to overlay

--- src/components/shared/overlay.js
+++ src/components/shared/overlay.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const Overlay = ({ children, className }) => (
   <div className={`bg-black-65 h-100 ${className}`}>{children}</div>
@@ -10,7 +10,7 @@ Overlay.GatsbyImage = ({ children, gatsbyImageData, alt, className="" }) => (
     <GatsbyImage
       style={{ gridArea: "1/1" }}
       className={className}
-      image={gatsbyImageData}
+      image={getImage(gatsbyImageData)}
       alt={alt}
     />
     <div style={{ gridArea: "1/1", position: "relative", display: "grid" }}>
