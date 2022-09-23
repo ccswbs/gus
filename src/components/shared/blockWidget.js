cb4d60b71413dfcffae4baf31625712683217cc2
Tanya Qureshi <tqureshi@uoguelph.ca>
2022-09-23 12:36:19 -0400

    Fix keys

--- src/components/shared/blockWidget.js
+++ src/components/shared/blockWidget.js
@@ -20,13 +20,13 @@ const BlockWidget = (props) => {
         return (widgetBlockContent.map(widget => {
             switch(widget.__typename) {
                 case "paragraph__accordion_section":
-                    return <Accordion pageData={widget} />;
+                    return <Accordion key={widget.drupal_id} pageData={widget} />;
                 case "paragraph__general_text":
-                    return <GeneralText processed={widget.field_general_text.processed} />;
+                    return <GeneralText key={widget.drupal_id} processed={widget.field_general_text.processed} />;
                 case "paragraph__media_text":
                     return <MediaText key={widget.drupal_id} widgetData={widget} />;
                 case "paragraph__section_tabs":
-                    return <PageTabs pageData={widget} />;
+                    return <PageTabs key={widget.drupal_id} pageData={widget} />;
                 case "paragraph__section_buttons":
                     return <SectionButtons key={widget.drupal_id} pageData={widget} />;
                 default:
