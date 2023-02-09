241e0da13915fcf408dd4c7ecd3c9d0fab8a04f4
Miranda Mafe <miranda.mafe@gmail.com>
2023-02-09 16:25:00 -0500

    Add South Asia Explore Form

--- src/components/shared/yamlWidget.js
+++ src/components/shared/yamlWidget.js
@@ -38,7 +38,7 @@ const YamlWidget = (props) => {
         'lang_bcomm_student_blog':<LangBcommStudentBlog />,
         'lang_bcomm_student_blog_blue':<LangBcommStudentBlog background="#F4F7FA" />,
         'south_asia_explore_grid': <SouthAsiaExploreGrid />,
-        'test_form': <SouthAsiaExploreForm />,
+        'south_asia_explore_form': <SouthAsiaExploreForm />,
 
     }[component] || null )
 }
