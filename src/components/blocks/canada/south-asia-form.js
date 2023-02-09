0f3157b67a33902b19e14fe1cb8f1951f4b95385
Miranda Mafe <miranda.mafe@gmail.com>
2023-02-09 16:14:47 -0500

    Add pagecontainer to form

--- src/components/blocks/canada/south-asia-form.js
+++ src/components/blocks/canada/south-asia-form.js
@@ -1,12 +1,17 @@
 import React from "react"
 import { Script } from "gatsby"
+import PageContainer from 'components/shared/pageContainer'
 
 const SouthAsiaExploreForm = () => {
     return (
-        <div className="ug-slate">
-            <div id="form_efcf8a53-bc40-40c9-836e-23edb1a7a0c4">Loading...</div>
-            <Script async="async" src="https://apply.uoguelph.ca/register/?id=efcf8a53-bc40-40c9-836e-23edb1a7a0c4&amp;output=embed&amp;div=form_efcf8a53-bc40-40c9-836e-23edb1a7a0c4">/**/</Script>
-        </div>
+        <PageContainer.SiteContent>
+            <PageContainer.ContentArea>
+                <div className="ug-slate">
+                    <div id="form_efcf8a53-bc40-40c9-836e-23edb1a7a0c4">Loading...</div>
+                    <Script async="async" src="https://apply.uoguelph.ca/register/?id=efcf8a53-bc40-40c9-836e-23edb1a7a0c4&amp;output=embed&amp;div=form_efcf8a53-bc40-40c9-836e-23edb1a7a0c4">/**/</Script>
+                </div>
+            </PageContainer.ContentArea>
+        </PageContainer.SiteContent>
 )}
 
 export default SouthAsiaExploreForm
