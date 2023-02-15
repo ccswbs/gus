4f6c0a48bda21b28c279e828aa04fe677a1be521
Tanya Qureshi <tqureshi@uoguelph.ca>
2023-02-15 16:39:19 -0500

    Remove most of impact report for test purposes

--- src/pages/grce/impact-report.js
+++ src/pages/grce/impact-report.js
@@ -7,13 +7,13 @@ import Seo from 'components/seo';
 import Breadcrumbs from 'components/shared/breadcrumbs';
 import EconImpactBanner from 'components/blocks/economic-impact/banner';
 import EconImpactPresMessage from 'components/blocks/economic-impact/pres-message'
-import EconImpactNationalImpact from 'components/blocks/economic-impact/national-impact';
+/* import EconImpactNationalImpact from 'components/blocks/economic-impact/national-impact';
 import EconImpactProvImpact from 'components/blocks/economic-impact/provincial-impact';
 import EconImpactCommunityImpact from 'components/blocks/economic-impact/community-impact';
 import EconImpactHumanImpact from 'components/blocks/economic-impact/human-impact';
 import EconImpactSustainability from 'components/blocks/economic-impact/sustainability';
 import EconImpactResearch from 'components/blocks/economic-impact/research';
-import EconImpactVolunteerism from 'components/blocks/economic-impact/volunteerism';
+import EconImpactVolunteerism from 'components/blocks/economic-impact/volunteerism'; */
 
 const render = ({ title, description, image, menu, domains }) => (
     <Layout menuName="grce-main">
@@ -22,13 +22,6 @@ const render = ({ title, description, image, menu, domains }) => (
         <EconImpactBanner />
         <Breadcrumbs menuName={menu} nodeTitle={title} domains={domains} />
         <EconImpactPresMessage />
-        <EconImpactNationalImpact />
-        <EconImpactProvImpact />
-        <EconImpactCommunityImpact />
-        <EconImpactHumanImpact />
-        <EconImpactSustainability />
-        <EconImpactResearch />
-        <EconImpactVolunteerism />
     </Layout>
 )
 
