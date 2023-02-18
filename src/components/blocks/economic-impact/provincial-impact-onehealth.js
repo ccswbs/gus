6bf835825ad38d63f0dbad9299e42a6462ccb55b
Tanya Qureshi <tqureshi@uoguelph.ca>
2023-02-17 22:22:37 -0500

    Refactor yaml calls

--- src/components/blocks/economic-impact/provincial-impact-onehealth.js
+++ src/components/blocks/economic-impact/provincial-impact-onehealth.js
@@ -2,41 +2,43 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import ModalVideo from "components/shared/modalVideo"
 
-const render = ({ title, body_html, video }) => (   
-  <>
-    <h3 className="mt-0 text-dark text-uppercase">{title}</h3>
+const yaml = require('js-yaml');
+
+const render = ({ field_yaml_map }) => {
+  let yamlMap;
+
+  try {
+    yamlMap = yaml.load(field_yaml_map);
+  } catch (e) {
+    console.log(e);
+    return null;
+  } 
+  
+  return (<>
+    <h3 className="mt-0 text-dark text-uppercase">{field_yaml_map.title}</h3>
     <div className="border-5 border-start px-4 mb-5">
-        <div dangerouslySetInnerHTML={{__html: body_html}}></div>
+        <div dangerouslySetInnerHTML={{__html: field_yaml_map.body_html}}></div>
         {video && 
           <ModalVideo 
-            id={video.id} 
-            src={video.url} 
-            title={video.title} 
-            transcript={video.transcript} 
+            id={field_yaml_map.video.id} 
+            src={field_yaml_map.video.url} 
+            title={field_yaml_map.video.title} 
+            transcript={field_yaml_map.video.transcript} 
             modalButton = {
                 <button type="button" className="btn btn-outline-info my-4">
-                    <i className="fa-solid fa-play"></i> Watch Video<span className="visually-hidden">: {video.title}</span>
+                    <i className="fa-solid fa-play"></i> Watch Video<span className="visually-hidden">: {field_yaml_map.video.title}</span>
                 </button>
             }
         />}
     </div>
-  </>
-)
-
+  </>)
+}
 const query = graphql`
   query {
     blockContentYamlBlock(yamlId: {eq: "economic_impact_provincial_onehealth"}) {
       id
-      title
-      body_html
-      video {
-        id
-        type
-        title
-        url
-        transcript
-        captions
-      }
+      field_yaml_id
+      field_yaml_map
     }
   }
 `
