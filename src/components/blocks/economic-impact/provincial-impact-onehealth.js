9686f461229972932b312a11c3af0a3068a4358f
Tanya Qureshi <tqureshi@uoguelph.ca>
2023-02-24 13:45:35 -0500

    Formatting fixes

--- src/components/blocks/economic-impact/provincial-impact-onehealth.js
+++ src/components/blocks/economic-impact/provincial-impact-onehealth.js
@@ -15,21 +15,23 @@ const render = ({ field_yaml_map }) => {
   } 
   
   return (<>
-    <h3 className="mt-0 text-dark text-uppercase">{yamlMap.title}</h3>
-    <div className="border-5 border-start px-4 mb-5">
-        <div dangerouslySetInnerHTML={{__html: yamlMap.body_html}}></div>
-        {yamlMap.video && 
-          <ModalVideo 
-            id={yamlMap.video.id} 
-            src={yamlMap.video.url} 
-            title={yamlMap.video.title} 
-            transcript={yamlMap.video.transcript} 
-            modalButton = {
-                <button type="button" className="btn btn-outline-info my-4">
-                    <i className="fa-solid fa-play"></i> Watch Video<span className="visually-hidden">: {yamlMap.video.title}</span>
-                </button>
-            }
-        />}
+    <div className="mt-4">
+        <h3 className="fs-3 mt-0 text-dark text-uppercase">{yamlMap.title}</h3>
+        <div className="uog-border-blue px-4 mb-5">
+            <div dangerouslySetInnerHTML={{__html: yamlMap.body_html}}></div>
+            {yamlMap.video && 
+              <ModalVideo 
+                id={yamlMap.video.id} 
+                src={yamlMap.video.url} 
+                title={yamlMap.video.title} 
+                transcript={yamlMap.video.transcript} 
+                modalButton = {
+                    <button type="button" className="btn btn-outline-info my-4">
+                        <i className="fa-solid fa-play"></i> Watch Video<span className="visually-hidden">: {yamlMap.video.title}</span>
+                    </button>
+                }
+            />}
+        </div>
     </div>
   </>)
 }
