55b08e631d69567d0d6e053e6a4c1e9a7ae6eb25
Tanya Qureshi <tqureshi@uoguelph.ca>
2023-06-06 16:23:10 -0400

    Add heading level field and logic

--- src/components/shared/accordion.js
+++ src/components/shared/accordion.js
@@ -8,23 +8,62 @@ const Accordion = (props) => {
     let dataParent = ("#accordion" + props.pageData?.drupal_id);
 
     if (accordionData) {
-        return <>
-            <div className="accordion" id={"accordion" + props.pageData.drupal_id}>
-            {accordionData.map(item =>
+      return (
+        <>
+          <div className="accordion" id={"accordion" + props.pageData.drupal_id}>
+            {accordionData.map((item) => {
+              const accordionToggle = (
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={"#part" + item.drupal_id}
+                  aria-expanded="false"
+                  aria-controls={"part" + item.drupal_id}
+                >
+                  {item?.field_accordion_title ? item.field_accordion_title : "Read More"}
+                </button>
+              );
+
+              let headingElement;
+
+              if (item.field_heading_level === "h2") {
+                headingElement = (
+                  <h2 className="accordion-header" id={"heading" + item.drupal_id}>
+                    {accordionToggle}
+                  </h2>
+                );
+              } else if (item.field_heading_level === "h3") {
+                headingElement = (
+                  <h3 className="accordion-header" id={"heading" + item.drupal_id}>
+                    {accordionToggle}
+                  </h3>
+                );
+              } else if (item.field_heading_level === "h4") {
+                headingElement = (
+                  <h4 className="accordion-header" id={"heading" + item.drupal_id}>
+                    {accordionToggle}
+                  </h4>
+                );
+              } else {
+                // Handle default case or error
+                headingElement = "h2";
+              }
+
+              return (
                 <div className="accordion-item" key={"item" + item.drupal_id}>
-                    <h2 className="accordion-header" id={"heading" + item.drupal_id}>
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#part" + item.drupal_id} aria-expanded="false" aria-controls={"part" + item.drupal_id}>
-                            {item?.field_accordion_title ? item.field_accordion_title : "Read More"}
-                        </button>
-                    </h2>
-                    <div {...(stayOpen ? {} : {"data-bs-parent":dataParent})} id={"part" + item.drupal_id} className="accordion-collapse collapse" aria-labelledby={"heading" + item.drupal_id}>            
-                        <div className="accordion-body" dangerouslySetInnerHTML={{__html: item.field_accordion_block_text.processed}} />
-                    </div>
+                  {headingElement}
+                  <div {...(stayOpen ? {} : {"data-bs-parent": dataParent})} id={"part" + item.drupal_id} className="accordion-collapse collapse" aria-labelledby={"heading" + item.drupal_id}>
+                    <div className="accordion-body" dangerouslySetInnerHTML={{__html: item.field_accordion_block_text.processed}} />
+                  </div>
                 </div>
-            )}
-            </div>
+              );
+            })}
+          </div>
         </>
+      );
     }
+
     return null;
 }
 
@@ -46,6 +85,7 @@ export const query = graphql`
       field_accordion_block_elements {
         drupal_id
         field_accordion_title
+        field_heading_level
         field_accordion_block_text {
           processed
         }        
