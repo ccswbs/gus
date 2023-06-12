75e7551cb297eff56be213c36b192eb24fffcbaf
Scott Schau <sschau@uoguelph.ca>
2023-06-12 14:08:02 -0400

    test issue with clean needed

--- src/templates/article-page.js
+++ src/templates/article-page.js
@@ -4,12 +4,14 @@ import Layout from 'components/layout';
 import Seo from 'components/seo';
 import InlineImage from 'components/shared/inlineImage';
 
-export default ({data}) => {
+const ArticlePage = ({data}) => {
 	const pageData = data.articles.edges[0].node;
 	const pageTitle = pageData.title;
+	const author = pageData.field_news_author;
+	const newsCategory = pageData.relationships.field_news_category;
 	const body = (pageData.body !== null ? pageData.body.processed:``);
 	const bodyInline = InlineImage(body); 
-
+console.log(newsCategory);
 	return (
 		<Layout>
 			<Seo title={pageTitle} keywords={[`gatsby`, `application`, `react`]} />
@@ -17,6 +19,7 @@ export default ({data}) => {
                 <div className="row site-content">
                     <div className="content-area">
                         <h1>{pageTitle}</h1>
+						<p>{author}</p>
 						<div className ="clearfix">
 							<div>{bodyInline}</div>
 						</div>
@@ -27,6 +30,8 @@ export default ({data}) => {
 	)
 }
 
+export default ArticlePage;
+
 export const query = graphql`
   query ($id: String) {
 	articles: allNodeArticle(filter: {id: {eq: $id}}) {
@@ -36,6 +41,18 @@ export const query = graphql`
 				title
 				body {
 					processed
+				}
+				field_news_author
+				relationships {
+					field_news_category {
+					  name
+					}
+					field_tags {
+					  ... on taxonomy_term__tags {
+						id
+						name
+					  }
+					}
 				}
 		  	}
 		}
