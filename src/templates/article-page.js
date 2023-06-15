66969ed94373bafe0fd2d6a9b20bd3f06db49521
Scott Schau <sschau@uoguelph.ca>
2023-06-15 12:50:28 -0400

    Changing display format- Issue: add footer

--- src/templates/article-page.js
+++ src/templates/article-page.js
@@ -3,15 +3,22 @@ import { graphql } from 'gatsby';
 import Layout from 'components/layout';
 import Seo from 'components/seo';
 import InlineImage from 'components/shared/inlineImage';
+import CustomFooter from 'components/shared/customFooter';
 
 const ArticlePage = ({data}) => {
 	const pageData = data.articles.edges[0].node;
 	const pageTitle = pageData.title;
 	const author = pageData.field_news_author;
+	const displayDate = "Posted: " + pageData.field_publish_date;
+	const updatedDate = "Updated: " + pageData.changed;
 	const newsCategory = pageData.relationships.field_news_category;
 	const body = (pageData.body !== null ? pageData.body.processed:``);
 	const bodyInline = InlineImage(body); 
-console.log(newsCategory);
+	const newsTopics = pageData.relationships.field_tags;
+	const footer = data.footer.edges;
+	console.log(data.footer.edges);
+console.log(newsCategory, "Cat");
+console.log(newsTopics, "Topic");
 	return (
 		<Layout>
 			<Seo title={pageTitle} keywords={[`gatsby`, `application`, `react`]} />
@@ -19,13 +26,20 @@ console.log(newsCategory);
                 <div className="row site-content">
                     <div className="content-area">
                         <h1>{pageTitle}</h1>
-						<p>{author}</p>
+						<p>
+						{author && <>By: {author}<br/></>}
+						{displayDate}<br/>
+						{updatedDate}</p>
 						<div className ="clearfix">
 							<div>{bodyInline}</div>
 						</div>
                     </div>
                 </div>
               </div>
+			  
+		  	{ /**** Custom Footer content ****/}
+			{footer?.length > 0 &&
+			<CustomFooter footerData={footer[0]} />}
 		</Layout>
 	)
 }
@@ -33,7 +47,7 @@ console.log(newsCategory);
 export default ArticlePage;
 
 export const query = graphql`
-  query ($id: String) {
+  query ($id: String, $tid: [String]) {
 	articles: allNodeArticle(filter: {id: {eq: $id}}) {
 		edges {
 			node {
@@ -43,9 +57,16 @@ export const query = graphql`
 					processed
 				}
 				field_news_author
+				field_publish_date (formatString: "MMMM DD, YYYY")
+				changed (formatString: "MMMM DD, YYYY")
 				relationships {
 					field_news_category {
 					  name
+					  relationships {
+						parent {
+						  name
+						}
+					  }
 					}
 					field_tags {
 					  ... on taxonomy_term__tags {
@@ -57,5 +78,12 @@ export const query = graphql`
 		  	}
 		}
 	}
+	footer: allNodeCustomFooter (filter: {fields: {tags: {in: $tid}}}){
+		edges {
+			node {
+			...CustomFooterFragment
+			}
+		}
+		}
   }
 `
