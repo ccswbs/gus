3aed51d29850451bd5178cca025151cd687f07fe
Raza Mahmood <razammahmood@gmail.com>
2023-10-17 15:58:31 -0400

    add script processing to generalText widget

--- src/components/shared/generalText.js
+++ src/components/shared/generalText.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
+import React from "react"
+import PropTypes from "prop-types"
+import { graphql, Script } from "gatsby"
+import { Parser, ProcessNodeDefinitions } from "html-to-react"
 
-const GeneralText = (props) => {    
-    let processed = props.processed;
-    let textClass = props.textClass;    
-    return <div {...(textClass !== `` ? {className:textClass} : {})} dangerouslySetInnerHTML={{__html: processed}}></div>    
+const GeneralText = (props) => {
+  const parser = new Parser()
+  const instructions = [
+    {
+      shouldProcessNode: (node) => node.name === "script",
+      processNode: (node) => <Script src={node?.attribs?.src} />,
+    },
+    {
+      shouldProcessNode: () => true,
+      processNode: new ProcessNodeDefinitions().processDefaultNode,
+    },
+  ]
+
+  const processed = parser.parseWithInstructions(props.processed, () => true, instructions)
+  const textClass = props.textClass
+
+  return <div {...(textClass !== `` ? { className: textClass } : {})}>{processed}</div>
 }
 
 GeneralText.propTypes = {
-    processed: PropTypes.string,
-    textClass: PropTypes.string,
+  processed: PropTypes.string,
+  textClass: PropTypes.string,
 }
 
 GeneralText.defaultProps = {
-    processed: ``,
-    textClass: ``,
+  processed: ``,
+  textClass: ``,
 }
 
 export default GeneralText
