b488dcf3cdb2b86fb80326a4dc155b67d314c034
Miranda Mafe <miranda.mafe@gmail.com>
2023-10-19 12:36:29 -0400

    Swap to useStaticQuery for admission-grid

--- src/components/blocks/admission/admission-grid.js
+++ src/components/blocks/admission/admission-grid.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import { Row, Col } from "react-bootstrap";
 import PageContainer from 'components/shared/pageContainer'
 import styled from "styled-components"
@@ -13,35 +13,6 @@ const HeadingIcon = styled.i`
   color: ${props => (props.iconColour ?? "#000000")};
 `
 
-const render = ({ field_yaml_map }) => {
-    let yamlMap;
-
-    try {
-      yamlMap = yaml.load(field_yaml_map);
-    } catch (e) {
-      console.log(e);
-      return null;
-    }
-    
-    return (
-      <Background>
-        <PageContainer.SiteContent>
-          <PageContainer.ContentArea>
-            {yamlMap.title && <h2 className="mt-4">{yamlMap.title}</h2>}
-            <Row className="row-cols-1 row-cols-md-3 g-4 mt-0">
-                {yamlMap.content.map(({title, body_html, icon, icon_color}, index) => 
-                    <Col key={`admission-supplementary-grid-${index}`} className="pe-4">
-                      <h3 className="h4 text-dark mt-0 d-flex align-items-start"><HeadingIcon aria-hidden={true} className={`${icon} pe-3`} iconColour={icon_color} /> {title}</h3>
-                      <div dangerouslySetInnerHTML={{__html: body_html}}></div>
-                    </Col>
-                )}
-            </Row>
-          </PageContainer.ContentArea>
-        </PageContainer.SiteContent>
-      </Background>
-)}
-
-
 const query = graphql`
   query {
     blockContentYamlBlock(field_yaml_id: {glob: "admission_supplementary_block"}) {
@@ -51,7 +22,35 @@ const query = graphql`
     }
   }
 `
-
+  
 export default function AdmissionGrid() {
-  return <StaticQuery query={query} render={({blockContentYamlBlock}) => render(blockContentYamlBlock)} />
+  const data = useStaticQuery(query);
+  const field_yaml_map = data.blockContentYamlBlock.field_yaml_map;
+  let yamlMap;
+
+  try {
+    yamlMap = yaml.load(field_yaml_map);
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+  
+  return (
+    <Background>
+      <PageContainer.SiteContent>
+        <PageContainer.ContentArea>
+          {yamlMap.title && <h2 className="mt-4">{yamlMap.title}</h2>}
+          <Row className="row-cols-1 row-cols-md-3 g-4 mt-0">
+              {yamlMap.content.map(({title, body_html, icon, icon_color}, index) => 
+                  <Col key={`admission-supplementary-grid-${index}`} className="pe-4">
+                    <h3 className="h4 text-dark mt-0 d-flex align-items-start"><HeadingIcon aria-hidden={true} className={`${icon} pe-3`} iconColour={icon_color} /> {title}</h3>
+                    <div dangerouslySetInnerHTML={{__html: body_html}}></div>
+                  </Col>
+              )}
+          </Row>
+        </PageContainer.ContentArea>
+      </PageContainer.SiteContent>
+    </Background>
+  )
+  
 }
