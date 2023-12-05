960c2bc4743a424e0a058425004e973921fd38d2
Scott Schau <sschau@uoguelph.ca>
2023-12-05 14:24:59 -0500

    sort faculty by tag

--- src/pages/ovcfaculty.js
+++ src/pages/ovcfaculty.js
@@ -2,52 +2,292 @@ import { graphql, Link } from 'gatsby';
 import Layout from 'components/layout';
 import React from 'react';
 import Seo from 'components/seo';
-import { GatsbyImage } from "gatsby-plugin-image";
+
 
 const IndexPage = ({ data }) => {
     console.log(data);
-    const pageTags = [];    
-    const pages = data.allNodePage.edges;
-    const tags = data.tags.edges;
-    
-    // Fetch tags used on pages
-    for (let i=0; i<tags.length; i++) {
-        if ((tags[i].node?.relationships?.node__page?.length > 0)&&(tags[i].node.name === "OVC Department of Biomedical Sciences" || tags[i].node.name === "OVC Department of Clinical Studies" ||tags[i].node.name === "OVC Department of Pathobiology" || tags[i].node.name === "OVC Department of Population Medicine" )){
-            pageTags.push(tags[i])            
+    const allNodePages = data.allNodePage.edges;
+    const ovcBioFaculty = [];
+    const ovcClinFaculty = [];
+    const ovcPathFaculty =[];
+    const ovcPopFaculty =[];
+
+    // Fetch OVC Faculty per department //
+    for (let i=0; i<allNodePages.length; i++) {
+      for (let j=0; j<allNodePages[i].node?.relationships?.field_tags.length; j++){
+        if ((allNodePages[i].node?.relationships?.field_tags.length > 0 ) && (allNodePages[i].node.relationships.field_tags[j].name === "OVC Department of Biomedical Sciences")){
+          ovcBioFaculty.push(allNodePages[i]);            
         }
-    }
-    console.log(pageTags)
- 
+      }
+    } 
+    for (let i=0; i<allNodePages.length; i++) {
+      for (let j=0; j<allNodePages[i].node?.relationships?.field_tags.length; j++){
+        if ((allNodePages[i].node?.relationships?.field_tags.length > 0 ) && (allNodePages[i].node.relationships.field_tags[j].name === "OVC Department of Clinical Studies")){
+          ovcClinFaculty.push(allNodePages[i]);            
+        }
+      }
+    } 
+    for (let i=0; i<allNodePages.length; i++) {
+      for (let j=0; j<allNodePages[i].node?.relationships?.field_tags.length; j++){
+        if ((allNodePages[i].node?.relationships?.field_tags.length > 0 ) && (allNodePages[i].node.relationships.field_tags[j].name === "OVC Department of Pathobiology")){
+          ovcPathFaculty.push(allNodePages[i]);            
+        }
+      }
+    } 
+    for (let i=0; i<allNodePages.length; i++) {
+      for (let j=0; j<allNodePages[i].node?.relationships?.field_tags.length; j++){
+        if ((allNodePages[i].node?.relationships?.field_tags.length > 0 ) && (allNodePages[i].node.relationships.field_tags[j].name ===  "OVC Department of Population Medicine")){
+          ovcPopFaculty.push(allNodePages[i]);            
+        }
+      }
+    } 
+
+    ovcBioFaculty.sort((a,b) => (a.node.title.split(' ').pop() > b.node.title.split(' ').pop()) ? 1 : ((b.node.title.split(' ').pop() > a.node.title.split(' ').pop()) ? -1 : 0));
+    ovcClinFaculty.sort((a,b) => (a.node.title.split(' ').pop() > b.node.title.split(' ').pop()) ? 1 : ((b.node.title.split(' ').pop() > a.node.title.split(' ').pop()) ? -1 : 0));
+    ovcPathFaculty.sort((a,b) => (a.node.title.split(' ').pop() > b.node.title.split(' ').pop()) ? 1 : ((b.node.title.split(' ').pop() > a.node.title.split(' ').pop()) ? -1 : 0));
+    ovcPopFaculty.sort((a,b) => (a.node.title.split(' ').pop() > b.node.title.split(' ').pop()) ? 1 : ((b.node.title.split(' ').pop() > a.node.title.split(' ').pop()) ? -1 : 0));
+   
+    // Fetch Adjunct, Emeritus, Departmental Faculty  for each department //
+
+    const ovcBioEmeritus = [];
+    const ovcBioAdjunct = [];
+    const ovcBioDepartment = [];
+
+    const ovcClinEmeritus = [];
+    const ovcClinAdjunct = [];
+    const ovcClinDepartment = [];
+    
+    const ovcPathEmeritus = [];
+    const ovcPathAdjunct = [];
+    const ovcPathDepartment = [];
+    
+    const ovcPopEmeritus = [];
+    const ovcPopAdjunct = [];
+    const ovcPopDepartment = [];
+
+    for (let i=0; i<ovcBioFaculty.length; i++) {
+      for (let j=0; j<ovcBioFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcBioFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcBioFaculty[i].node.relationships.field_tags[j].name === "OVC Professor Emeritus")){
+          ovcBioEmeritus.push(ovcBioFaculty[i]);            
+        }
+      }
+    } 
+    for (let i=0; i<ovcBioFaculty.length; i++) {
+      for (let j=0; j<ovcBioFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcBioFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcBioFaculty[i].node.relationships.field_tags[j].name === "OVC Adjunct Faculty")){
+          ovcBioAdjunct.push(ovcBioFaculty[i]);            
+        }
+      }
+    } 
+    for (let i=0; i<ovcBioFaculty.length; i++) {
+      for (let j=0; j<ovcBioFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcBioFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcBioFaculty[i].node.relationships.field_tags[j].name === "OVC Department Faculty")){
+          ovcBioDepartment.push(ovcBioFaculty[i]);            
+        }
+      }
+    } 
+    
+    for (let i=0; i<ovcClinFaculty.length; i++) {
+      for (let j=0; j<ovcClinFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcClinFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcClinFaculty[i].node.relationships.field_tags[j].name === "OVC Professor Emeritus")){
+          ovcClinEmeritus.push(ovcClinFaculty[i]);            
+        }
+      }
+    } 
+    for (let i=0; i<ovcClinFaculty.length; i++) {
+      for (let j=0; j<ovcClinFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcClinFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcClinFaculty[i].node.relationships.field_tags[j].name === "OVC Adjunct Faculty")){
+          ovcClinAdjunct.push(ovcClinFaculty[i]);            
+        }
+      }
+    } 
+    for (let i=0; i<ovcClinFaculty.length; i++) {
+      for (let j=0; j<ovcClinFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcClinFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcClinFaculty[i].node.relationships.field_tags[j].name === "OVC Department Faculty")){
+          ovcClinDepartment.push(ovcClinFaculty[i]);            
+        }
+      }
+    } 
+
+    for (let i=0; i<ovcPathFaculty.length; i++) {
+      for (let j=0; j<ovcPathFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcPathFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcPathFaculty[i].node.relationships.field_tags[j].name === "OVC Professor Emeritus")){
+          ovcPathEmeritus.push(ovcPathFaculty[i]);            
+        }
+      }
+    } 
+    for (let i=0; i<ovcPathFaculty.length; i++) {
+      for (let j=0; j<ovcPathFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcPathFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcPathFaculty[i].node.relationships.field_tags[j].name === "OVC Adjunct Faculty")){
+          ovcPathAdjunct.push(ovcPathFaculty[i]);            
+        }
+      }
+    } 
+    for (let i=0; i<ovcPathFaculty.length; i++) {
+      for (let j=0; j<ovcPathFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcPathFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcPathFaculty[i].node.relationships.field_tags[j].name === "OVC Department Faculty")){
+          ovcPathDepartment.push(ovcPathFaculty[i]);            
+        }
+      }
+    } 
+
+    for (let i=0; i<ovcPopFaculty.length; i++) {
+      for (let j=0; j<ovcPopFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcPopFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcPopFaculty[i].node.relationships.field_tags[j].name === "OVC Professor Emeritus")){
+          ovcPopEmeritus.push(ovcPopFaculty[i]);            
+        }
+      }
+    } 
+    for (let i=0; i<ovcPopFaculty.length; i++) {
+      for (let j=0; j<ovcPopFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcPopFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcPopFaculty[i].node.relationships.field_tags[j].name === "OVC Adjunct Faculty")){
+          ovcPopAdjunct.push(ovcPopFaculty[i]);            
+        }
+      }
+    } 
+    for (let i=0; i<ovcPopFaculty.length; i++) {
+      for (let j=0; j<ovcPopFaculty[i].node?.relationships?.field_tags.length; j++){
+        if ((ovcPopFaculty[i].node?.relationships?.field_tags.length > 0 ) && (ovcPopFaculty[i].node.relationships.field_tags[j].name === "OVC Department Faculty")){
+          ovcPopDepartment.push(ovcPopFaculty[i]);            
+        }
+      }
+    } 
+  
     return (
     <Layout>
         <Seo title="Ontario Veterinary College" />
         <div className="container page-container">
           <div className="site-content">
             <div className="content-area">
-              <h1>Ontario Veterinary College Faculty List</h1>              
-              {pageTags.map((tag) => {
-                const taggedPages = tag.node.relationships.node__page;
-                const taggedPagesPubbed = taggedPages.filter(page => page.status === true);
-                console.log("taggedPagesPubbed",taggedPagesPubbed)
-                taggedPagesPubbed.sort((a,b) => (a.title.split(' ').pop() > b.title.split(' ').pop()) ? 1 : ((b.title.split(' ').pop() > a.title.split(' ').pop()) ? -1 : 0));
-                return (taggedPagesPubbed.length > 0 && 
-                  <React.Fragment key={`tagged-fragment-${tag.node.name}`}>
-                    <h2 className="text-dark" id={`${tag.node.name.replace("OVC ",'').replace(/\s+/g, '-',).toLowerCase()}`}>{tag.node.name}</h2>
-                    {tag.node.description?.processed && <div dangerouslySetInnerHTML={{__html: tag.node.description.processed}}></div>}
-                    <p>Total pages: <strong>{taggedPagesPubbed.length}</strong></p>
-                      <ul className="three-col-md">
-                        {taggedPagesPubbed.map((taggedPage, index) => (
-                            <li key={`tagged-${index}`}><Link to={taggedPage.path.alias}>
-                            {/* <GatsbyImage image={taggedPage.relationships.field_widgets.relationships.field_media_text_media.relationships.field_media_image} alt="" /> */}
-                            {console.log("field_widgets",taggedPage.relationships.field_widgets[0].relationships.field_media_text_media)}
-                            {taggedPage.title}
-                            </Link></li>
-                        ))}
-                      </ul>
-                  </React.Fragment>)
-              })}
+              <h1>Ontario Veterinary College Faculty List</h1>        
+              <h2 className='text-dark' id="biomedical-sciences">Department of Biomedical Sciences</h2> 
+              <h3 className='text-dark'>Departmental Faculty</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcBioDepartment.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+                <h3 className='text-dark'>Professor Emeritus/Emerita</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcBioEmeritus.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+                <h3 className='text-dark'>Adjunct Faculty Faculty</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcBioAdjunct.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+                
+                
+              <h2 className='text-dark' id="clinical-studies">Department of Clinical Studies</h2> 
+              <h3 className='text-dark'>Departmental Faculty</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcClinDepartment.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+                <h3 className='text-dark'>Professor Emeritus/Emerita</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcClinEmeritus.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+                <h3 className='text-dark'>Adjunct Faculty Faculty</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcClinAdjunct.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+              <h2 className='text-dark' id="pathobiology">Department of Pathobiology</h2> 
+              <h3 className='text-dark'>Departmental Faculty</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcPathDepartment.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+                <h3 className='text-dark'>Professor Emeritus/Emerita</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcPathEmeritus.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+                <h3 className='text-dark'>Adjunct Faculty Faculty</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcPathAdjunct.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+              <h2 className='text-dark' id="population-medicine">Department of Population Medicine</h2>  
+              <h3 className='text-dark'>Departmental Faculty</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcPopDepartment.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+                <h3 className='text-dark'>Professor Emeritus/Emerita</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcPopEmeritus.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
+                <h3 className='text-dark'>Adjunct Faculty Faculty</h3>
+                <ul className="three-col-md">
+                  {
+                    ovcPopAdjunct.map((faculty) => {
+                      return (
+                        <li key={faculty.node.drupal_id}><Link to = {faculty.node.path.alias}>{faculty.node.title} </Link></li>
+                      )
+                    })
+                  }
+                </ul>
             </div>
-          </div>ß
+          </div>
         </div>
     </Layout>
     )
@@ -56,13 +296,6 @@ const IndexPage = ({ data }) => {
 export default IndexPage
 
 export const query = graphql`{
-  accordion: homeYaml(yamlId: {eq: "home_accordion"}) {
-    id
-    accordion {
-      title
-      content
-    }
-  }
   allNodePage(sort: {title: ASC}) {
     edges {
       node {
@@ -82,78 +315,6 @@ export const query = graphql`{
           }
         }
         status
-      }
-    }
-  }
-  programs: allNodeProgram(sort: {title: ASC}) {
-    edges {
-      node {
-        drupal_id
-        drupal_internal__nid
-        title
-        path {
-          alias
-        }
-        status
-      }
-    }
-  }
-  tags: allTaxonomyInterface(sort: {name: ASC}) {
-    edges {
-      node {
-        ... on taxonomy_term__tags {
-          name
-          description {
-            processed
-          }
-          relationships {
-            node__page {
-              status
-              title
-              path {
-                alias
-              }
-              relationships {
-                field_widgets {
-                  ... on paragraph__media_text {
-                    id
-                    relationships {
-                      field_media_text_media {
-                        ... on media__image {
-                          id
-                          name
-                          field_media_image {
-                            alt
-                          }
-                          relationships {
-                            field_media_image {
-                              gatsbyImage(cropFocus: FACES, placeholder: BLURRED, width: 500, aspectRatio: 1)
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-        ... on taxonomy_term__units {
-          name
-          description {
-            processed
-          }
-          relationships {
-            node__page {
-              status
-              title
-              path {
-                alias
-              }
-            }
-          }
-        }
       }
     }
   }
