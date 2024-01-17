974ab8c9fa49a8fc114849aa80400c6d6a4e2e98
Raza Mahmood <razammahmood@gmail.com>
2024-01-17 12:32:48 -0500

    add logic to open tab based on link

--- src/components/shared/pageTabs.js
+++ src/components/shared/pageTabs.js
@@ -1,29 +1,44 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import PropTypes from "prop-types"
 import { graphql } from "gatsby"
 import Tab from "react-bootstrap/Tab"
 import Tabs from "react-bootstrap/Tabs"
-import Fade from 'react-bootstrap/Fade';
-import 'styles/pageTabs.css';
+import Fade from "react-bootstrap/Fade"
+import "styles/pageTabs.css"
+import { slugify } from "../../utils/ug-utils"
 
 const PageTabs = (props) => {
+  const container = useRef(null)
   const tabs = props.pageData?.relationships?.field_tabs ?? []
   const [activeTab, setActiveTab] = useState("")
 
   useEffect(() => {
-    let initialTab = tabs[0]?.drupal_id;
-    setActiveTab(initialTab)
+    const params = new URLSearchParams(window.location.search)
+    const title = params.get("tab")
+    let tabID = tabs[0]?.drupal_id
+
+    if (title) {
+      for (const tab of tabs) {
+        if (title === slugify(tab.field_tab_title.toLowerCase())) {
+          tabID = tab.drupal_id
+          container.current?.scrollIntoView({ block: "start", inline: "nearest" })
+          break
+        }
+      }
+    }
+
+    setActiveTab(tabID)
   }, [])
-  
+
   if (tabs.length <= 0) {
     return null
   }
 
   return (
-    <div className="col-md-12 content-area nav-tabs-container">
+    <div className="col-md-12 content-area nav-tabs-container" ref={container}>
       <Tabs activeKey={activeTab} onSelect={(k) => setActiveTab(k)} justify transition={Fade}>
         {tabs.map((tab) => (
-          <Tab key={tab.drupal_id} eventKey={tab.drupal_id} title={tab.field_tab_title}>
+          <Tab key={tab.drupal_id} eventKey={tab.drupal_id} title={tab.field_tab_title.toUpperCase()}>
             <div dangerouslySetInnerHTML={{ __html: tab.field_tab_body.processed }} />
           </Tab>
         ))}
