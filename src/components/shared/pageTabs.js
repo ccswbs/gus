31a82147a53337269aaf0fb98c97a1489f4924d6
Raza Mahmood <razammahmood@gmail.com>
2024-01-17 11:05:08 -0500

    refactor pageTabs.js

--- src/components/shared/pageTabs.js
+++ src/components/shared/pageTabs.js
@@ -1,93 +1,60 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
-import { contentExists } from 'utils/ug-utils.js';
-import NavTabs from 'components/shared/navTabs';
-import NavTabHeading from 'components/shared/navTabHeading';
-import NavTabContent from 'components/shared/navTabContent';
+import React, { useState, useEffect } from "react"
+import PropTypes from "prop-types"
+import { graphql } from "gatsby"
+import Tab from "react-bootstrap/Tab"
+import Tabs from "react-bootstrap/Tabs"
+import 'styles/pageTabs.css';
 
-function renderTabInfo (pageData) {
-    
-    let activeValue = true;
-    let navTabHeadings = [];
-    let navTabContent = [];    
+const PageTabs = (props) => {
+  const tabs = props.pageData?.relationships?.field_tabs ?? []
+  const [activeTab, setActiveTab] = useState("")
 
-    if (contentExists(pageData)) {
+  useEffect(() => {
+    let initialTab = tabs[0]?.drupal_id;
+    setActiveTab(initialTab)
+  }, [])
+  
+  if (tabs.length <= 0) {
+    return null
+  }
 
-        let tabID = "";
-
-        for (let i = 0; i < pageData.relationships.field_tabs.length; i++) {
-            tabID = "pills-" + pageData.relationships.field_tabs[i].drupal_id;
-
-            if (i > 0) activeValue = false;
-
-            navTabHeadings.push(<NavTabHeading key={`navTabHeading-` + i}
-                active={activeValue}
-                heading={pageData.relationships.field_tabs[i].field_tab_title}
-                controls={tabID}
-            />);
-
-            navTabContent.push(<NavTabContent key={`navTabContent-` + i}
-                active={activeValue}
-                id={tabID}
-                content={<div dangerouslySetInnerHTML={{ __html: pageData.relationships.field_tabs[i].field_tab_body.processed }} />}
-            />);
-        }
-        return (
-        <React.Fragment>
-            <NavTabs headings={
-                navTabHeadings.map((heading) => {
-                    return heading;
-                    })
-                }>
-                {navTabContent.map((content) => {
-                    return content;
-
-                })}
-            </NavTabs>
-        </React.Fragment>
-        )
-    }
-    return null;
+  return (
+    <div className="col-md-12 content-area">
+      <Tabs activeKey={activeTab} onSelect={(k) => setActiveTab(k)} justify>
+        {tabs.map((tab) => (
+          <Tab key={tab.drupal_id} eventKey={tab.drupal_id} title={tab.field_tab_title}>
+            <div dangerouslySetInnerHTML={{ __html: tab.field_tab_body.processed }} />
+          </Tab>
+        ))}
+      </Tabs>
+    </div>
+  )
 }
 
-function pageTabs (props) {
-
-    if (contentExists(props.pageData) && props.pageData.length !== 0) {        
-        return (
-            <div className="col-md-12 content-area">
-                {renderTabInfo(props.pageData)}
-           </div>
-        )
-    } else {
-        return null;
-    }
+PageTabs.propTypes = {
+  pageData: PropTypes.object,
 }
 
-pageTabs.propTypes = {
-    pageData: PropTypes.object,
+PageTabs.defaultProps = {
+  pageData: ``,
 }
 
-pageTabs.defaultProps = {
-    pageData: ``,
-}
-
-export default pageTabs
+export default PageTabs
 
 export const query = graphql`
-    fragment SectionTabsParagraphFragment on paragraph__section_tabs {
+  fragment SectionTabsParagraphFragment on paragraph__section_tabs {
+    drupal_id
+    relationships {
+      field_tabs {
         drupal_id
-        relationships {
-          field_tabs {
-            drupal_id
-            field_tab_title
-            field_tab_body {
-              processed
-            }
-          }
-          field_section_column {
-            name
-          }
+        field_tab_title
+        field_tab_body {
+          processed
         }
+      }
+      field_section_column {
+        name
+      }
     }
+  }
 `
