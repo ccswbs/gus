33156beb3b29640fc14f98f7c2ec89f8f36a1760
Tanya Qureshi <tqureshi@uoguelph.ca>
2024-02-08 19:45:06 -0500

    Refactor and replace tabs with spaces

--- src/components/shared/testimonial.js
+++ src/components/shared/testimonial.js
@@ -7,100 +7,98 @@ import TestimonialTitle from 'components/shared/testimonialTitle';
 import { stripHTMLTags, setHeadingLevel } from 'utils/ug-utils.js';
 import 'styles/testimonial.css';
 
-function setTestimonialHeading (props) {
-	// heading passed through prop
-	if( props.heading ) {
-		return props.heading;
-	}
+function setTestimonialHeading(props) {
+    // heading passed through prop
+    if (props.heading) {
+        return props.heading;
+    }
 
-	// heading generated for program page
-	if ( props.programAcronym ) {
-		const testimonialTags = props.testimonialData.flatMap((testim) => {
-			let tag = TestimonialTitle(testim.node ?? testim);
-			return tag ?? [];
-		})
+    // heading generated for program page
+    if (props.programAcronym) {
+        
+        const testimonialTags = props.testimonialData.flatMap(testim => TestimonialTitle(testim.node ?? testim) ?? []);
 
-		let studentTag = testimonialTags.includes('Graduate Student', 'Undergraduate Student')
-		let facultyTag = testimonialTags.includes('Faculty')
-		let alumniTag = testimonialTags.includes('Alumni')
-	
-		// Testimonial heading generated for program pages
-		let testimonialHeading = studentTag && facultyTag && alumniTag ? "Alumni, Students, and Faculty from the program" : 
-			studentTag && !facultyTag && !alumniTag ? "Students from the program" :
-			studentTag && facultyTag && !alumniTag ? "Students and Faculty from the program":
-			studentTag && !facultyTag && alumniTag ? "Alumni and Students from the program":
-			!studentTag && facultyTag && alumniTag ? "Alumni and Faculty from the program":
-			!studentTag && facultyTag && !alumniTag ? "Faculty from the program" :
-			!studentTag && !facultyTag && alumniTag ? "Alumni from the program" : "Students in the program"
-	
-		return ("What " + testimonialHeading + " are saying about U of G");
-	}
+        const studentTag = testimonialTags.includes('Graduate Student', 'Undergraduate Student')
+        const facultyTag = testimonialTags.includes('Faculty')
+        const alumniTag = testimonialTags.includes('Alumni')
+    
+        // Testimonial heading generated for program pages
+        let testimonialHeading = studentTag && facultyTag && alumniTag ? "Alumni, Students, and Faculty" : 
+            studentTag && !facultyTag && !alumniTag ? "Students from" :
+            studentTag && facultyTag && !alumniTag ? "Students and Faculty from":
+            studentTag && !facultyTag && alumniTag ? "Alumni and Students from":
+            !studentTag && facultyTag && alumniTag ? "Alumni and Faculty from":
+            !studentTag && facultyTag && !alumniTag ? "Faculty from" :
+            !studentTag && !facultyTag && alumniTag ? "Alumni from" : "Students in"
+    
+        return ("What " + testimonialHeading + " the program are saying about U of G");
+    }
 }
 
 function Testimonials (props) {
-	let Heading = setHeadingLevel(props.headingLevel);
-	let testimonialHeading = setTestimonialHeading(props)
+    let Heading = setHeadingLevel(props.headingLevel);
+    let testimonialHeading = setTestimonialHeading(props)
 
-	if (props.testimonialData?.length > 0) {
-		const testimonialUnits  = () => props.testimonialData.map((testimonial) => {
-			// program queries are nested under .node and widget queries are not
-			let testimonialNode = testimonial.node ?? testimonial;
-			let testimonialName = testimonialNode.field_testimonial_person_name ?? testimonialNode.title;
-			let testimonialTitle = TestimonialTitle(testimonialNode);
-			let testimonialContent = stripHTMLTags(testimonialNode.body.processed);
-			let testimonialPicture = getImage(testimonialNode.relationships.field_hero_image?.relationships.field_media_image);
+    if (props.testimonialData?.length > 0) {
+        const testimonialUnits  = () => props.testimonialData.map((testimonial) => {
+            // program queries are nested under .node and widget queries are not
+            let testimonialNode = testimonial.node ?? testimonial;
+            let testimonialName = testimonialNode.field_testimonial_person_name ?? testimonialNode.title;
+            let testimonialTitle = TestimonialTitle(testimonialNode);
+            let testimonialContent = stripHTMLTags(testimonialNode.body.processed);
+            let testimonialPicture = getImage(testimonialNode.relationships.field_hero_image?.relationships.field_media_image);
     
-			const testimonialHomeProfile = () => {
-				let testimonialHomeProfileTitle = (testimonialNode.field_home_profile?.title === "") ? "Associated Profile" : testimonialNode.field_home_profile?.title;
-				let testimonialHomeProfileLink = testimonialNode.field_home_profile?.uri;
+            const testimonialHomeProfile = () => {
+                let testimonialHomeProfileTitle = (testimonialNode.field_home_profile?.title === "") ? "Associated Profile" : testimonialNode.field_home_profile?.title;
+                let testimonialHomeProfileLink = testimonialNode.field_home_profile?.uri;
 
-				if (testimonialHomeProfileLink) {
-					return <a href= {testimonialHomeProfileLink}>{testimonialHomeProfileTitle}</a>
-				} 
-				return null;
-			};
+                if (testimonialHomeProfileLink) {
+                    return <a href= {testimonialHomeProfileLink}>{testimonialHomeProfileTitle}</a>
+                } 
+                return null;
+            };
 
-			return (
-				<div key={testimonialNode.drupal_id}>
-					{testimonialPicture && 
-						<GatsbyImage image={testimonialPicture}
-							className="testimonial-pic"
-							alt={testimonialNode.relationships.field_hero_image.field_media_image.alt} />}
-					<blockquote className="testimonial-quote mb-0" dangerouslySetInnerHTML={{__html: testimonialContent}} />
-					<p className="testimonial-tagline author">
-						<strong className="testimonial-title">{testimonialName}{testimonialTitle ? ', ' + testimonialTitle : '' }</strong>
-						<br />
-						<span className="testimonial-person-desc">{testimonialNode.field_testimonial_person_desc}</span>
-						<br/>
-						{testimonialNode.field_home_profile && testimonialHomeProfile()}
-					</p>
-				</div>
-		);
-		})
-		return (
-			<div className="ug-testimonial">
-				<div className="full-width-container bg-light">
-					<div className="container page-container">
-						<section className="row row-with-vspace site-content">
-							<div className="col-md-12 content-area">
-								{testimonialHeading && <Heading className="carousel-header text-dark">{testimonialHeading}</Heading>}
-								<div className="testimonial-wrapper">
-									<SliderResponsive addToControlLabel="Testimonial">{testimonialUnits()}</SliderResponsive>
-								</div>
-							</div>
-						</section>
-					</div>
-				</div>
-			</div>
-		)
+            return (
+                <div key={testimonialNode.drupal_id}>
+                    {testimonialPicture && 
+                        <GatsbyImage image={testimonialPicture}
+                            className="testimonial-pic"
+                            alt={testimonialNode.relationships.field_hero_image.field_media_image.alt} />}
+                    <blockquote className="testimonial-quote mb-0" dangerouslySetInnerHTML={{__html: testimonialContent}} />
+                    <p className="testimonial-tagline author">
+                        <strong className="testimonial-title">{testimonialName}{testimonialTitle ? ', ' + testimonialTitle : '' }</strong>
+                        <br />
+                        <span className="testimonial-person-desc">{testimonialNode.field_testimonial_person_desc}</span>
+                        <br/>
+                        {testimonialNode.field_home_profile && testimonialHomeProfile()}
+                    </p>
+                </div>
+        );
+        })
+        return (
+            <div className="ug-testimonial">
+                <div className="full-width-container bg-light">
+                    <div className="container page-container">
+                        <section className="row row-with-vspace site-content">
+                            <div className="col-md-12 content-area">
+                                {testimonialHeading && <Heading className="carousel-header text-dark">{testimonialHeading}</Heading>}
+                                <div className="testimonial-wrapper">
+                                    <SliderResponsive addToControlLabel="Testimonial">{testimonialUnits()}</SliderResponsive>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        )
 
-	} else {
-		return null
-	}
+    } else {
+        return null
+    }
 }
 
 Testimonials.propTypes = {
-	testimonialData: PropTypes.array,
+    testimonialData: PropTypes.array,
 }
 
 Testimonials.defaultProps = {
@@ -109,51 +107,50 @@ Testimonials.defaultProps = {
 
 export default Testimonials
 
-
 export const query = graphql`
-	fragment TestimonialNodeFragment on node__testimonial {
-		changed
-		drupal_id
-		body {
-			processed
-		}
-		title
-		field_testimonial_person_name
-		field_testimonial_person_desc
-		field_home_profile {
-			title
-			uri
-		}
-		relationships {
-			field_hero_image {
-				field_media_image {
-					alt
-				}
-				relationships {
-					field_media_image {
-						gatsbyImage(
-							width: 400
-							height: 400
-							placeholder: BLURRED
-							layout: CONSTRAINED
-							formats: [AUTO, WEBP]
-						)
-					}
-				}
-			}
-			field_testimonial_type {
-				drupal_id
-				id
-				name
-			}
-			field_tags {
-				__typename
-				... on TaxonomyInterface {
-					drupal_id
-					id
-					name
-				}
-			}
-		}
-	}
+  fragment TestimonialNodeFragment on node__testimonial {
+    changed
+    drupal_id
+    body {
+      processed
+    }
+    title
+    field_testimonial_person_name
+    field_testimonial_person_desc
+    field_home_profile {
+      title
+      uri
+    }
+    relationships {
+      field_hero_image {
+        field_media_image {
+          alt
+        }
+        relationships {
+          field_media_image {
+            gatsbyImage(
+              width: 400
+              height: 400
+              placeholder: BLURRED
+              layout: CONSTRAINED
+              formats: [AUTO, WEBP]
+            )
+          }
+        }
+      }
+      field_testimonial_type {
+        drupal_id
+        id
+        name
+      }
+      field_tags {
+        __typename
+        ... on TaxonomyInterface {
+          drupal_id
+          id
+          name
+        }
+      }
+    }
+  }
 `
