b288406690eefc8501126a5c48a1afa02bbc9e3f
Tanya Qureshi <tqureshi@uoguelph.ca>
2024-03-14 16:53:33 -0400

    Add shouldProcessNode function for file links

--- src/components/shared/generalText.js
+++ src/components/shared/generalText.js
@@ -1,10 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { graphql, Script } from "gatsby"
+import { graphql, Script, useStaticQuery } from "gatsby"
 import { Parser, ProcessNodeDefinitions } from "html-to-react"
 
 const GeneralText = (props) => {
-  const parser = new Parser()
+    
+  const urlData = useStaticQuery(graphql`
+    query {
+      sitePlugin(name: {eq: "gatsby-source-drupal"}) {
+        pluginOptions
+      }
+    }
+  `)
+  const baseUrl = urlData.sitePlugin.pluginOptions.baseUrl;
+  const parser = new Parser();
+  
   const instructions = [
     {
       // Replace <script> tags with Gatsby <Script> components
@@ -15,6 +25,24 @@ const GeneralText = (props) => {
       // Inline scripts will also be replaced but should only have one child, which is the script text
       shouldProcessNode: (node) => node.name === "script" && node?.children.length === 1,
       processNode: (node) => <Script>{node.children[0].data}</Script>,
+    },
+    {
+      // Process anchor tags to prepend URL and remove data-entity attributes
+      shouldProcessNode: (node) => node.name === "a",
+      processNode: (node, children) => {
+      // Prepend the URL to the href attribute
+      let newAttribs = {...node.attribs, href: baseUrl + node.attribs.href};
+
+      // Remove the data-entity attributes
+        for (let attr in newAttribs) {
+          if (attr.startsWith('data-entity')) {
+            delete newAttribs[attr];
+          }
+        }
+
+        // Return a new anchor tag with the modified attributes and the original children
+        return <a {...newAttribs}>{children}</a>;
+      },
     },
     {
       // Process all other nodes with the default parser
