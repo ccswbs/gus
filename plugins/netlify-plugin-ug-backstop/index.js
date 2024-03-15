5a96f7235e0eaa30a229a82cbf19b3e6d43a568b
Miranda Mafe <miranda.mafe@gmail.com>
2024-03-15 19:29:09 -0400

    Add reference step

--- plugins/netlify-plugin-ug-backstop/index.js
+++ plugins/netlify-plugin-ug-backstop/index.js
@@ -2,6 +2,7 @@ const backstop = require('backstopjs');
 
 module.exports = {
   onSuccess: () => {
+    backstop('reference', {config:'backstop.config.js'});
     backstop('test', {config:'backstop.config.js'});
   },
 }
