cfec812c632b105e9d6c3e3f1eeb132f5253a357
Miranda Mafe <miranda.mafe@gmail.com>
2024-03-15 17:08:00 -0400

    Test on preBuild

--- plugins/netlify-plugin-ug-backstop/index.js
+++ plugins/netlify-plugin-ug-backstop/index.js
@@ -1,7 +1,12 @@
 // index.js
 
 module.exports = {
-  onEnd: () => {
-    console.log("Hello world from onEnd event!");
+  onPreBuild: () => {
+    console.log("-------------------------------------------------------");
+    console.log("Testing URL: " + process.env.BACKSTOPJS_URL);
+    console.log("Reference URL: " + process.env.BACKSTOPJS_REF);
+    console.log("-------------------------------------------------------");
+
+    backstop('test', {config:'../backstop.config.js'});
   },
 }
