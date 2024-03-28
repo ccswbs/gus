f12c94d493f1b6786a06f88638bce5a72a0b1cd4
Tanya Qureshi <tqureshi@uoguelph.ca>
2024-03-28 11:50:56 -0400

    Remove unneeded call to contentExists function

--- src/components/shared/stats.js
+++ src/components/shared/stats.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { contentExists} from 'utils/ug-utils';
 import 'styles/stats.css';
 
 function Stats (props) {
 	return (
 		
 		<React.Fragment>
-			{contentExists(props.statsData) && props.statsData.length !== 0 && <>
+			{props.statsData && props.statsData.length !== 0 && <>
 				{props.statsData.map (stat => {	
 					let statRange = stat.field_stat_range;
 					let statType = stat.relationships.field_stat_type.name;
@@ -18,7 +17,7 @@ function Stats (props) {
 					return <React.Fragment key={stat.drupal_id}>
 					<div className="uog-card text-break">
 						<dt>
-							{contentExists(statFAIcon) === true && <span className="fa-icon-colour"><i className={statFAIcon}>  </i></span>}
+							{statFAIcon && <span className="fa-icon-colour"><i className={statFAIcon}>  </i></span>}
 							{statRange === true && statValueEnd !== null ? statValue + " - " + statValueEnd : statValue}
 						</dt>
 						<dd>{statType}</dd>
