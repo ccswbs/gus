616855dc8a984861aa66c1d13a198d9d6d0f8d23
Miranda Mafe <miranda.mafe@gmail.com>
2024-07-16 11:43:44 -0400

    Filter out queue_for_delete content from queries

--- gatsby-node.js
+++ gatsby-node.js
@@ -1037,7 +1037,7 @@ exports.createPages = async ({ graphql, actions, createNodeId, reporter }) => {
 
   const result = await graphql(`
     {
-      pages: allNodePage(filter: { path: { alias: { ne: null } }, moderation_state: { ne: "archived" } }) {
+      pages: allNodePage(filter: { path: { alias: { ne: null } }, moderation_state: { ne: ["archived","queue_for_delete"] } }) {
         edges {
           node {
             id
@@ -1053,7 +1053,7 @@ exports.createPages = async ({ graphql, actions, createNodeId, reporter }) => {
           }
         }
       }
-      programs: allNodeProgram(filter: { path: { alias: { ne: null } }, moderation_state: { ne: "archived" } }) {
+      programs: allNodeProgram(filter: { path: { alias: { ne: null } }, moderation_state: { ne: ["archived","queue_for_delete"] } }) {
         edges {
           node {
             title
