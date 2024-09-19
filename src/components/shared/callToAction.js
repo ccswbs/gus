89f827fe1480bc23d98bcfe1ddfa4d000df9669a
Tanya Qureshi <tqureshi@uoguelph.ca>
2024-09-19 12:06:22 -0400

    Convert callToAction to GTM custom events

--- src/components/shared/callToAction.js
+++ src/components/shared/callToAction.js
@@ -1,42 +1,47 @@
-import PropTypes from 'prop-types'
-import React from 'react';
-import { trackCustomEvent } from 'gatsby-plugin-google-analytics';
+import PropTypes from "prop-types";
+import React from "react";
 
 const CallToAction = ({ children, classes, href, goalEventCategory, goalEventAction }) => {
-    if (goalEventCategory) {
-        return(
-            <a className={classes} href={href} onClick={e => {
-                // Track the custom click
-                trackCustomEvent({
-                  category: goalEventCategory,
-                  action: goalEventAction,
-                })
-              }}>
-                {children}
-            </a>
-        )
-    }
-    return(
-        <a className={classes} href={href}>
-            {children}
-        </a>
-    )
-}
+  if (goalEventCategory) {
+    return (
+      <a
+        className={classes}
+        href={href}
+        onClick={(e) => {
+          // Track the custom click
+          window.dataLayer = window.dataLayer || [];
+          window.dataLayer.push({
+            event: "custom_event",
+            category: goalEventCategory,
+            action: goalEventAction,
+          });
+        }}
+      >
+        {children}
+      </a>
+    );
+  }
+  return (
+    <a className={classes} href={href}>
+      {children}
+    </a>
+  );
+};
 
 CallToAction.propTypes = {
-    children: PropTypes.node.isRequired,
-    classes: PropTypes.string,
-    goalEventAction: PropTypes.string,
-    goalEventCategory: PropTypes.string,
-    href: PropTypes.string,
-}
-  
-CallToAction.defaultProps = {
-    children: ``,
-    classes: "btn btn-primary fs-1 me-4 p-4 w-100",
-    href: "#",
-    goalEventAction: ``,
-    goalEventCategory: ``,
-}
+  children: PropTypes.node.isRequired,
+  classes: PropTypes.string,
+  goalEventAction: PropTypes.string,
+  goalEventCategory: PropTypes.string,
+  href: PropTypes.string,
+};
 
-export default CallToAction
+CallToAction.defaultProps = {
+  children: ``,
+  classes: "btn btn-primary fs-1 me-4 p-4 w-100",
+  href: "#",
+  goalEventAction: ``,
+  goalEventCategory: ``,
+};
+
+export default CallToAction;
