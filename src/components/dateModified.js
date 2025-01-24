ffd5c3c66036539543b66e46a31cab31f74e694f
Miranda Mafe <miranda.mafe@gmail.com>
2025-01-23 19:59:59 -0500

    Use dayjs instead of moment

--- src/components/dateModified.js
+++ src/components/dateModified.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import moment from 'moment-timezone'
+// import moment from 'moment-timezone'
+import dayjs from 'dayjs';
+
 
 function DateModified ({ date }) {
-    const lastModified = moment(date).tz('America/Toronto').format(`MMMM D, YYYY HH:mm z`);
+    // const lastModified = moment(date).tz('America/Toronto').format(`MMMM D, YYYY HH:mm z`);
+    const lastModified = dayjs(date).format(`MMMM D, YYYY HH:mm z`);
     if(date !== null && date !== ""){
         return <div className="container-fluid date-modified">
                     <div className="container ft-container">
