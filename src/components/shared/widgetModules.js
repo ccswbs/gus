77f7a71173f6c3829c626f4cde0555555b245a2a
Miranda Mafe <miranda.mafe@gmail.com>
2025-02-05 23:43:03 -0500

    Remove lazyload from general text

--- src/components/shared/widgetModules.js
+++ src/components/shared/widgetModules.js
@@ -1,7 +1,7 @@
 const widgetModules = {
   'paragraph__accordion_section': { container: 'container', moduleName: 'accordion', shouldLazyLoad: true, fallback: null, shouldRenderPrimary: true, shouldRenderSecondary: true, shouldRenderBlock: true },
   'paragraph__block_widget': { container: 'container', moduleName: 'blockWidget', shouldLazyLoad: true, fallback: null, shouldRenderPrimary: true, shouldRenderSecondary: true, shouldRenderBlock: false },
-  'paragraph__general_text': { container: 'container', moduleName: 'generalText', shouldLazyLoad: true, fallback: null, shouldRenderPrimary: true, shouldRenderSecondary: true, shouldRenderBlock: true },
+  'paragraph__general_text': { container: 'container', moduleName: 'generalText', shouldLazyLoad: false, fallback: null, shouldRenderPrimary: true, shouldRenderSecondary: true, shouldRenderBlock: true },
   'paragraph__image_overlay': { container: 'container-fluid', moduleName: 'imageOverlay', shouldLazyLoad: true, fallback: null, shouldRenderPrimary: true, shouldRenderSecondary: false, shouldRenderBlock: false },
   'paragraph__lead_paragraph': { container: 'container', moduleName: 'leadPara', shouldLazyLoad: true, fallback: null, shouldRenderPrimary: true, shouldRenderSecondary: false, shouldRenderBlock: false },
   'paragraph__links_widget': { container: 'container', moduleName: 'linksWidget', shouldLazyLoad: true, fallback: null, shouldRenderPrimary: true, shouldRenderSecondary: false, shouldRenderBlock: false },
