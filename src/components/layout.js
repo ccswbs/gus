fce5780e38fdcc30d2c725712830f1e4e4287ea7
Miranda Mafe <miranda.mafe@gmail.com>
2025-02-07 12:49:30 -0500

    Remove font import

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import HeaderMenu from 'components/shared/headerMenu';
 import 'styles/global.css';
-import "@fontsource/roboto"
-import "@fontsource/roboto/700.css"
-import "@fontsource/roboto/300.css"
-import "@fontsource/roboto/100-italic.css"
-import "@fontsource/roboto/400-italic.css"
 
 const Layout = ({ children, menuName }) => (
   <>
