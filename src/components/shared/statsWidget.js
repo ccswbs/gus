a36f87332f0ccffc7d7584287848c405082d8f24
Miranda Mafe <miranda.mafe@gmail.com>
2025-02-07 21:07:13 -0500

    Fix console warning

--- src/components/shared/statsWidget.js
+++ src/components/shared/statsWidget.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
-import Stats from 'components/shared/stats'
 
 function StatsWidget (props) {
-	
 	let statsData = props.data.relationships.field_statistic;
 
-	return (<>
-		<dl className="card-group stats">
-			<Stats statsData={statsData} />
-		</dl>
-	</>)
+  if(statsData && statsData.length !== 0){
+    return (
+      <dl className="card-group stats">
+        {statsData.map (stat => {	
+          let statRange = stat.field_stat_range;
+          let statType = stat.relationships.field_stat_type.name;
+          let statValue = stat.field_stat_value;
+          let statValueEnd = stat.field_stat_value_end;
+          let statFAIcon = stat.field_font_awesome_icon;
+  
+          return <div key={`stat--${statType}`} className="uog-card text-break">
+              <dt>
+                {statFAIcon && <span className="fa-icon-colour"><i className={statFAIcon}>  </i></span>}
+                {statRange === true && statValueEnd !== null ? statValue + " - " + statValueEnd : statValue}
+              </dt>
+              <dd>{statType}</dd>
+            </div>
+        })}
+      </dl>
+    )
+  }
 }
 		
 StatsWidget.propTypes = {
