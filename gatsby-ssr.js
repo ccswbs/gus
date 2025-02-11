9165350a97142aa778a7b946e663d914ab1251df
Miranda Mafe <miranda.mafe@gmail.com>
2025-02-10 21:55:53 -0500

    Test specific fonts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,10 +9,10 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
     <link rel="preconnect" href="https://fonts.gstatic.com" key="https://fonts.gstatic.com" crossOrigin="anonymous" />,
 
     // Roboto and Roboto Condensed
-    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" as="style" crossOrigin="anonymous" />,    
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:ital,wght@0,100;0,400;1,300;1,700&display=swap" as="style" crossOrigin="anonymous" />,    
     <link 
-      href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" 
-      key="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" 
+      href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:ital,wght@0,100;0,400;1,300;1,700&display=swap" 
+      key="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:ital,wght@0,100;0,400;1,300;1,700&display=swap" 
       rel="stylesheet" />,
 
     // Web Components
