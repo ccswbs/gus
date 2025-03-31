193aadf7d384c25fdd58d6dcc85f5e17f9e4f029
Tanya Qureshi <tqureshi@uoguelph.ca>
2025-03-31 14:33:59 -0400

    prettier formatting

--- src/components/shared/customFooter.js
+++ src/components/shared/customFooter.js
@@ -1,56 +1,55 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import React, { lazy, Suspense } from "react";
-import { graphql } from 'gatsby';
+import { graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
-import Widget from 'components/shared/widget';
-import { ParseText } from "utils/ug-utils"
-import 'styles/customFooter.css';
+import Widget from "components/shared/widget";
+import { ParseText } from "utils/ug-utils";
+import "styles/customFooter.css";
 
-const OVCCustomFooter = lazy(() => import('components/blocks/ovc/footerOVC'));
+const OVCCustomFooter = lazy(() => import("components/blocks/ovc/footerOVC"));
 
-function chooseFooter (footer) {
+function chooseFooter(footer) {
   let footerSelection = ``;
-  footer.node.relationships?.field_tags.forEach(
-    tag => {
-      if(tag.__typename === "taxonomy_term__units" && 
-        tag.name.includes("Ontario Veterinary College")){
-          footerSelection = "OVC";
-      }
+  footer.node.relationships?.field_tags.forEach((tag) => {
+    if (tag.__typename === "taxonomy_term__units" && tag.name.includes("Ontario Veterinary College")) {
+      footerSelection = "OVC";
     }
-  )
+  });
   return footerSelection;
 }
 
-const DefaultFooter = ({logos, text, widgets}) => (
+const DefaultFooter = ({ logos, text, widgets }) => (
   <div className="full-width-container bg-light pre-footer">
-  <div className="container page-container">
-    <section className="row row-with-vspace site-content">
-      {logos &&
-        <div className="col-md-3 content-area">
-          {logos.map(logo => (
-            <GatsbyImage
-              key={`footer-logo-${logo.field_footer_logo?.drupal_id}`}
-              image={logo.relationships?.field_media_image?.gatsbyImage}
-              className="footer-logo"
-              alt={logo.field_media_image?.alt} />
-          ))}
-        </div>}
-      <div className="col-md-9 content-area">
-        <div className="container">
-          <ParseText textContent={text} />
+    <div className="container page-container">
+      <section className="row row-with-vspace site-content">
+        {logos && (
+          <div className="col-md-3 content-area">
+            {logos.map((logo) => (
+              <GatsbyImage
+                key={`footer-logo-${logo.field_footer_logo?.drupal_id}`}
+                image={logo.relationships?.field_media_image?.gatsbyImage}
+                className="footer-logo"
+                alt={logo.field_media_image?.alt}
+              />
+            ))}
+          </div>
+        )}
+        <div className="col-md-9 content-area">
+          <div className="container">
+            <ParseText textContent={text} />
+          </div>
+          <Widget data={widgets} />
         </div>
-        <Widget data={widgets} />
-      </div>
-    </section>			
+      </section>
+    </div>
   </div>
-</div>
-)
+);
 
 const CustomFooter = (props) => {
   const footer = props.footerData;
-	const footerLogos = footer.node.relationships?.field_footer_logo ? footer.node.relationships.field_footer_logo : null;
-	const footerText = footer.node.body?.processed ? footer.node.body.processed : null;
-	const footerWidgets = footer.node.relationships?.field_widgets ? footer.node.relationships.field_widgets : null;
+  const footerLogos = footer.node.relationships?.field_footer_logo ? footer.node.relationships.field_footer_logo : null;
+  const footerText = footer.node.body?.processed ? footer.node.body.processed : null;
+  const footerWidgets = footer.node.relationships?.field_widgets ? footer.node.relationships.field_widgets : null;
 
   switch (chooseFooter(footer)) {
     case "OVC":
@@ -58,28 +57,24 @@ const CustomFooter = (props) => {
         <Suspense fallback={<></>}>
           <OVCCustomFooter footerData={footer} />
         </Suspense>
-      )
+      );
     default:
-      return <DefaultFooter
-              logos={footerLogos} 
-              text={footerText} 
-              widgets={footerWidgets} />
+      return <DefaultFooter logos={footerLogos} text={footerText} widgets={footerWidgets} />;
   }
-}
+};
 
-		
 CustomFooter.propTypes = {
-    footerData: PropTypes.object,
-}
+  footerData: PropTypes.object,
+};
 CustomFooter.defaultProps = {
-    footerData: null,
-}
+  footerData: null,
+};
 
-export default CustomFooter
+export default CustomFooter;
 
 export const query = graphql`
-	fragment CustomFooterFragment on node__custom_footer {
-		drupal_id
+  fragment CustomFooterFragment on node__custom_footer {
+    drupal_id
     body {
       processed
     }
@@ -111,5 +106,5 @@ export const query = graphql`
         ...FieldWidgetsFragment
       }
     }
-	}
-`
+  }
+`;
